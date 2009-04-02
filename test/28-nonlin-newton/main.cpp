e35878ae06f1d50e3b9882dbfffc76ce11d3588f
jakub <jakub@dbed12e5-3d52-4894-8360-3e909281c254>
2009-04-02 07:49:22 +0000

    putting back test/28-nonlin-newton
    git-svn-id: svn+ssh://spilka.math.unr.edu/hermes2d/trunk@158 dbed12e5-3d52-4894-8360-3e909281c254

--- test/28-nonlin-newton/main.cpp
+++ test/28-nonlin-newton/main.cpp
@@ -1,135 +1,89 @@
 // 
 //  Nonlinear solver test:
 //
-//  PDE: non stationary heat transfer with nonlinear thermal conductuvity
-//  dT/dt - div[lambda(T)grad T] = 0
+//  PDE:  -\Delta u + u^3 = 0
 //
-//  Domain: square
+//  Domain: annulus centered at origin, with inner radius A = 0.05 and
+//  outer radius B = 0.5.
 //
-//  BC:  T = 100 on the left, top and bottom edges
-//       dT/dn = 0 on the right edge
+//  BC:  u = 1/A  on inner boundary
+//       du/dn = -1/B^2  on outer boundary
 //
-
-// time step and number of time steps
-double TAU = 0.1;
-int NSTEP = 100;
-
-// thermal conductivity (temperature-dependent)
-// for any u, this function has to be  positive in the entire domain!
-double lam(double T) { return 1.0 + T*T; } 
-double dlam_dT(double T) { return 2*T; }
+//  Exact solution:  u = 1 / sqrt(x^2 + y^2)
+//
 
 #include "hermes2d.h"
 #include "solver_umfpack.h"
 
+const double A = 0.05;
+const double B = 0.5;
+
+
+Solution uprev;
+
+#include "forms.h"
+
+scalar residual(RealFunction* vi, RefMap* ri)
+{
+  return int_grad_U_grad_v(&uprev, vi, ri, ri) +
+         int_u3_v(&uprev, vi, ri, ri);
+}
+
+scalar residual_surf(RealFunction* vi, RefMap* ri, EdgePos* ep)
+{
+  return 1.0/(sqr(B)) * surf_int_v(vi, ri, ep);
+}
+
+
+scalar jacobian(RealFunction* vj, RealFunction* vi, RefMap* rj, RefMap* ri)
+{
+  RefMap* ru = uprev.get_refmap();
+  return int_grad_u_grad_v(vj, vi, rj, ri) +
+         3*int_u2_vj_vi(&uprev, vj, vi, ru, rj, ri);
+}
+
+
 int bc_types(int marker)
 {
-  if (marker == 1 || marker == 3 || marker == 4) return BC_ESSENTIAL;
-  else return BC_NATURAL;
+  if (marker == 1)
+    return BC_ESSENTIAL;
+  return BC_NATURAL;
 }
 
 scalar bc_values(int marker, double x, double y)
 {
-  if (marker == 1 || marker == 3 || marker == 4) return 100.; 
+  if (marker == 1) return 1.0 / A; 
 }
 
-Solution Tprev, // previous time step solution, for the implicit Euler method
-         Titer; // solution converging during the Newton's iteration
-
-inline double F(RealFunction* Tprev, RealFunction* Titer, RealFunction* fu, RefMap* ru)
+scalar exact(double x, double y, scalar& dx, scalar& dy)
 {
-  Quad2D* quad = fu->get_quad_2d();
-
-  int o = Titer->get_fn_order() + fu->get_fn_order() 
-          + ru->get_inv_ref_order() + 4;
-  limit_order(o);
-  Titer->set_quad_order(o, FN_DEFAULT);
-  fu->set_quad_order(o, FN_DEFAULT);
-
-  double* Titer_val = Titer->get_fn_values();
-  double* Tprev_val = Tprev->get_fn_values();
-  double* uval = fu->get_fn_values();
-  double *dTiter_dx, *dTiter_dy, *dudx, *dudy;
-  Titer->get_dx_dy_values(dTiter_dx, dTiter_dy);
-  fu->get_dx_dy_values(dudx, dudy);
-
-  double result = 0.0; 
-  double3* pt = quad->get_points(o); 
-  int np = quad->get_num_points(o); 
-  double2x2 *mv, *mu; 
-  mu = ru->get_inv_ref_map(o); 
-  double* jac = ru->get_jacobian(o); 
-  for (int i = 0; i < np; i++, mu++, mv++) {
-    result += pt[i][2] * jac[i] * ( (Titer_val[i] - Tprev_val[i])*uval[i]/TAU + 
-				    lam(Titer_val[i]) * (dTiter_dx[i]*t_dudx + dTiter_dy[i]*t_dudy)); 
-  }
-  return result;
+  return 1.0 / hypot(x, y);
 }
 
-scalar linear_form_0(RealFunction* fv, RefMap* rv)
-{ return F(&Tprev, &Titer, fv, rv); }
 
-inline double J(RealFunction* Titer, RealFunction* fu, 
-                RealFunction* fv, RefMap* ru, RefMap* rv)
-{
-  Quad2D* quad = fu->get_quad_2d();
-
-  int o = Titer->get_fn_order() + fu->get_fn_order() 
-          + fv->get_fn_order() + ru->get_inv_ref_order() + 4;
-  limit_order(o);
-  Titer->set_quad_order(o, FN_DEFAULT);
-  fu->set_quad_order(o, FN_DEFAULT);
-  fv->set_quad_order(o, FN_DEFAULT);
-
-  double* Titer_val = Titer->get_fn_values();
-  double* uval = fu->get_fn_values();
-  double* vval = fv->get_fn_values();
-  double *dTiter_dx, *dTiter_dy, *dudx, *dudy, *dvdx, *dvdy;
-  Titer->get_dx_dy_values(dTiter_dx, dTiter_dy);
-  fu->get_dx_dy_values(dudx, dudy);
-  fv->get_dx_dy_values(dvdx, dvdy);
-
-  double result0 = 0.0; 
-  double3* pt = quad->get_points(o); 
-  int np = quad->get_num_points(o); 
-  double2x2 *mv, *mu; 
-  mu = ru->get_inv_ref_map(o); 
-  mv = rv->get_inv_ref_map(o); 
-  double* jac = ru->get_jacobian(o); 
-  for (int i = 0; i < np; i++, mu++, mv++) {
-    result0 += pt[i][2] * jac[i] * (vval[i] * uval[i]/TAU +
-      dlam_dT(Titer_val[i]) * uval[i] * (dTiter_dx[i]*t_dvdx + dTiter_dy[i]*t_dvdy)
-      + lam(Titer_val[i]) * (t_dudx*t_dvdx + t_dudy*t_dvdy)); 
-  }
-
-  return result0;
-}
-
-scalar bilinear_form_0_0(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
-{ return J(&Titer, fu, fv, ru, rv); }
-
-
-// *************************************************************
 int main(int argc, char* argv[])
 {
   Mesh mesh;
-  mesh.load("square.mesh");
-  for(int i=0; i<5; i++) mesh.refine_all_elements();
-  
+  mesh.load("annulus.mesh");
+  mesh.refine_all_elements(1);
+  mesh.refine_all_elements();
+  mesh.refine_towards_boundary(1, 3);
+  mesh.refine_all_elements();
+  mesh.refine_all_elements();
+
   H1Shapeset shapeset;
   PrecalcShapeset pss(&shapeset);
   
   H1Space space(&mesh, &shapeset);
   space.set_bc_types(bc_types);
   space.set_bc_values(bc_values);
-  space.set_uniform_order(1);
+  space.set_uniform_order(3);
   space.assign_dofs();
   
   WeakForm wf(1);
-  //wf.add_biform(0, 0, jacobian, UNSYM, ANY, 1, &Tprev);
-  //wf.add_liform(0, residual, ANY, 1, &Tprev);
-  wf.add_biform(0, 0, bilinear_form_0_0, UNSYM, ANY, 1, &Titer);
-  wf.add_liform(0, linear_form_0, ANY, 2, &Titer, &Tprev);
+  wf.add_biform(0, 0, jacobian, UNSYM, ANY, 1, &uprev);
+  wf.add_liform(0, residual, ANY, 1, &uprev);
+  wf.add_liform_surf(0, residual_surf, 2);
   
   UmfpackSolver umfpack;
   NonlinSystem nls(&wf, &umfpack);
@@ -137,47 +91,38 @@ int main(int argc, char* argv[])
   nls.set_pss(1, &pss);
   
   ScalarView view("Iteration", 0, 0, 880, 800);
-  //ScalarView errview("Error", 900, 0, 880, 800);
+  ScalarView errview("Error", 900, 0, 880, 800);
   
-  // initial condition at zero time level
-  Tprev.set_const(&mesh, 0.0);
-  //view.show(&Tprev);    
-  //View::wait();
-  nls.set_ic(&Tprev, &Tprev);
-  //view.show(&Tprev);    
-  //View::wait();
-  view.show(&uprev);
-  view.wait_for_keypress();
+  // uprev must contain the dirichlet lift at the beginning
+  uprev.set_const(&mesh, 0.0);
+  nls.set_ic(&uprev, &uprev);
 
-  // time stepping
-  for(int n = 1; n<=NSTEP; n++) {
-
-    info("\n---- Time step %d -----------------------------------------------", n);
+  int it = 1;
+  double l2e;
+  do
+  {
+    info("\n---- Iteration %d ---------------------------------------\n", it++);
     
-    // initial condition for the Newton's iteration
-    Titer.copy(&Tprev);
-
-    int it = 1; double res_l2_norm; 
-    do
-    {
-      info("\n---- Time step %d, iter %d ---------------------------------------\n", n, it++);
+    Solution sln;
+    nls.assemble();
+    nls.solve(1, &sln);
+    info("Residuum L2 norm: %g\n", nls.get_residuum_l2_norm());
     
-      Solution sln;
-      nls.assemble();
-      nls.solve(1, &sln);
-
-      res_l2_norm = nls.get_residuum_l2_norm(); 
-      info("Residuum L2 norm: %g\n", res_l2_norm);
-      view.show(&sln);    
-      Titer = sln;
-      View::wait();
-    }
-    while (res_l2_norm > 1e-4);
-
-    // copying result of the Newton's iteration into Tprev
-    Tprev = Titer;
-  }  
-
-  //View::wait();
+    view.show(&sln);
+    
+    ExactSolution exsln(&mesh, exact);
+    DiffFilter err(&sln, &exsln);
+    errview.show(&err);
+//     errview.wait_for_keypress();
+    
+    l2e = l2_error(&sln, &exsln);
+    info("L2 error: %g", l2e);
+    
+    uprev = sln;
+  }
+  while (nls.get_residuum_l2_norm() > 1e-4);
+  //while (l2e > 1e-5);
+  
+  View::wait();
   return 0;
 }
