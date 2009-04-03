da9b28efc1defb021d03879d3093c8f35ba6e977
lenka <lenka@dbed12e5-3d52-4894-8360-3e909281c254>
2009-04-03 08:54:10 +0000

    sync
    git-svn-id: svn+ssh://spilka.math.unr.edu/hermes2d/trunk@161 dbed12e5-3d52-4894-8360-3e909281c254

--- test/29-nonlin-fixed/main.cpp
+++ test/29-nonlin-fixed/main.cpp
@@ -1,99 +1,132 @@
 // 
-// 
 //  Nonlinear solver test:
 //
-//  PDE: non stationary heat transfer with nonlinear thermal conductuvity
-//  dT/dt - div[lambda(T)grad T] = 0
+//  PDE:  -\Delta u + u^3 = 0
 //
-//  Domain: square
+//  Domain: annulus centered at origin, with inner radius A = 0.05 and
+//  outer radius B = 0.5.
 //
-//  BC:  T = 100 on the left, top and bottom edges
-//       dT/dn = 0 on the right edge
+//  BC:  u = 1/A  on inner boundary
+//       du/dn = -1/B^2  on outer boundary
+//
+//  Exact solution:  u = 1 / sqrt(x^2 + y^2)
 //
 
-//#define DEBUG_ORDER
+#define DEBUG_ORDER
 
 #include "hermes2d.h"
 #include "solver_umfpack.h"
 
-// time step and number of time steps
-double TAU = 1e-7;
-int NSTEP = 50;
-
-// thermal conductivity (temperature-dependent)
-// for any u, this function has to be  positive in the entire domain!
-double lam(double T) { return 1.0 + T*T; } 
-double dlam_dT(double T) { return 2*T; }
+const double A = 0.05;
+const double B = 0.5;
 
 
-int bc_types(int marker)
-{
-  if (marker == 4 || marker == 1 || marker == 3) return BC_ESSENTIAL;
-//   if (marker == 4) return BC_ESSENTIAL;
-  else return BC_NATURAL;
-}
-
-scalar bc_values(int marker, double x, double y)
-{
-  if (marker == 4 || marker == 1 || marker == 3) return 100;
-//   if (marker == 4) return -4.0 * sqr(y) + 4.0 * y; 
-  else return 0.0;
-}
-
-inline double int_lambda_grad_u_grad_v(RealFunction* w, RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
+inline double int_w2_u_v(RealFunction* w, RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
 {
   Quad2D* quad = fu->get_quad_2d();
 
-  int o = fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order() + 2*w->get_fn_order();
+  int o = 2*w->get_fn_order() + fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
   limit_order(o);
-  fu->set_quad_order(o);
-  fv->set_quad_order(o);
-  w->set_quad_order(o);
+  w->set_quad_order(o, FN_VAL);
+  fu->set_quad_order(o, FN_VAL);
+  fv->set_quad_order(o, FN_VAL);
 
-  double *dudx, *dudy, *dvdx, *dvdy;
-  fu->get_dx_dy_values(dudx, dudy);
-  fv->get_dx_dy_values(dvdx, dvdy);
-  double* wval = w->get_fn_values();
-  
-  h1_integrate_dd_expression(lam(wval[i]) * (t_dudx * t_dvdx + t_dudy * t_dvdy));
+  double *wval = w->get_fn_values();  
+  double *vval = fv->get_fn_values();
+  double *uval = fu->get_fn_values();
+
+  h1_integrate_expression(wval[i] * wval[i] * uval[i] * vval[i]);
+  return result;
+}
+
+inline double int_w3_v(RealFunction* w, RealFunction* fu, RefMap* ru)
+{
+  Quad2D* quad = fu->get_quad_2d();
+
+  int o = 3*w->get_fn_order() + fu->get_fn_order() + ru->get_inv_ref_order();
+  limit_order(o);
+  w->set_quad_order(o, FN_VAL);
+  fu->set_quad_order(o, FN_VAL);
+
+  double *wval = w->get_fn_values();  
+  double *vval = fu->get_fn_values();
+
+  h1_integrate_expression(wval[i] * wval[i] * wval[i] * vval[i]);
   return result;
 }
 
 
-Solution Tprev, Titer;
+Solution uprev;
 
 scalar bilinear_form(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
 {
-  return int_lambda_grad_u_grad_v(&Titer, fu, fv, ru, rv) + int_u_v(fu, fv, ru, rv) / TAU;
+  return int_grad_u_grad_v(fu, fv, ru, rv) + int_w2_u_v(&uprev, fu, fv, ru, rv);
 }
 
-scalar linear_form(RealFunction* fv, RefMap* rv)
+scalar linear_form_surf(RealFunction* fv, RefMap* rv, EdgePos* ep)
 {
-  return int_w_v(&Tprev, fv, rv) / TAU;
+  return -1.0/(sqr(B)) * surf_int_v(fv, rv, ep);
 }
- 
+
+// scalar bilinear_form(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
+// {
+//   return int_grad_u_grad_v(fu, fv, ru, rv);
+// }
+// 
+// scalar linear_form(RealFunction* fv, RefMap* rv)
+// {
+//   return -int_w3_v(&uprev, fv, rv);
+// }
+//  
+// scalar linear_form_surf(RealFunction* fv, RefMap* rv, EdgePos* ep)
+// {
+//   return -1.0/(sqr(B)) * surf_int_v(fv, rv, ep);
+// }
+
+
+
+int bc_types(int marker)
+{
+  if (marker == 1)
+    return BC_ESSENTIAL;
+  return BC_NATURAL;
+}
+
+scalar bc_values(int marker, double x, double y)
+{
+  if (marker == 1) return 1.0 / A; 
+  else return 1.0 / B;
+}
+
+scalar exact(double x, double y, scalar& dx, scalar& dy)
+{
+  return 1.0 / sqrt(sqr(x) + sqr(y));
+}
+
 
 int main(int argc, char* argv[])
 {
   Mesh mesh;
-  mesh.load("square.mesh");
+  mesh.load("annulus.mesh");
+  mesh.refine_all_elements(1);
   mesh.refine_all_elements();
+  mesh.refine_towards_boundary(1, 3);
   mesh.refine_all_elements();
-  mesh.refine_all_elements();
-  mesh.refine_all_elements();
+//  mesh.refine_all_elements();
 
   H1Shapeset shapeset;
   PrecalcShapeset pss(&shapeset);
   
   H1Space space(&mesh, &shapeset);
-  space.set_bc_types(bc_types);
+  //space.set_bc_types(bc_types);
   space.set_bc_values(bc_values);
-  space.set_uniform_order(1);
+  space.set_uniform_order(3);
   space.assign_dofs();
   
   WeakForm wf(1);
-  wf.add_biform(0, 0, bilinear_form, UNSYM, ANY, 1, &Titer);
-  wf.add_liform(0, linear_form, ANY, 1, &Tprev);
+  wf.add_biform(0, 0, bilinear_form, UNSYM, ANY, 1, &uprev);
+  //wf.add_liform(0, linear_form, ANY, 1, &uprev);
+  //wf.add_liform_surf(0, linear_form_surf, 2);
   
   UmfpackSolver umfpack;
   LinSystem ls(&wf, &umfpack);
@@ -101,38 +134,44 @@ int main(int argc, char* argv[])
   ls.set_pss(1, &pss);
   
   ScalarView view("Iteration", 0, 0, 880, 800);
+  ScalarView errview("Error", 900, 0, 880, 800);
   
-  Tprev.set_zero(&mesh);
-  Titer.set_zero(&mesh);
-  
+  //uprev.set_exact(&mesh, exact);
+  uprev.set_zero(&mesh);
 
-  for(int n = 1; n <= NSTEP; n++) 
+  ExactSolution exsln(&mesh, exact);
+  view.show(&exsln);
+  view.wait_for_keypress();
+
+  GnuplotGraph graph;
+  graph.set_captions("L2 error","iterations","error [%]");
+  graph.add_row("fixed point");
+
+  double residuum, error;  
+  int it = 0;
+  do
   {
+    info("------------------ it = %d------------------------------", it++);
 
-    info("\n---- Time step %d -----------------------------------------------", n);
+    Solution sln;
+    ls.assemble();
 
-    double residuum, error;  
-    int it = 1;
-    do
-    {
-      info("------------------ it = %d------------------------------", it++);
-  
-      Solution sln;
-      ls.assemble();  
-      ls.solve(1, &sln);
-      info("residuum: %g", residuum = l2_error(&Titer, &sln)); 
-      
-  
-      view.show(&sln);
-      view.wait_for_keypress();
-  
-      Titer = sln;
-    }
-    while (residuum > 1e-4);
+    ls.solve(1, &sln);
+    info("residuum: %g", residuum = l2_error(&uprev, &sln)); 
+    info("error: %g", error = l2_error(&sln, &exsln));
+    info("");
     
-    Tprev.copy(&Titer);
+//     graph.add_values(0, it, error);
+//     graph.save("conv.txt");
+
+    view.show(&sln);
+    DiffFilter err(&sln, &exsln);
+    errview.show(&err);
+    //errview.wait_for_keypress();
+
+    uprev = sln;
   }
-  
+  while (error > 1e-5);
   
   View::wait();
   return 0;
