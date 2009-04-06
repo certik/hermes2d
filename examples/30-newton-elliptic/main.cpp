1ac57671c060b050748d1e9fa7e56c254a095b8c
svn <svn@dbed12e5-3d52-4894-8360-3e909281c254>
2009-04-06 01:01:48 +0000

    Example 30-newton-elliptic polished.
    git-svn-id: svn+ssh://spilka.math.unr.edu/hermes2d/trunk@165 dbed12e5-3d52-4894-8360-3e909281c254

--- examples/30-newton-elliptic/main.cpp
+++ examples/30-newton-elliptic/main.cpp
@@ -2,215 +2,171 @@
 #include "solver_umfpack.h"
 #include "function.h"
 
-// This class performs the Newton's method 
-// Y_{n+1} = Y_n - NEWTON_ALPHA*J^{-1}(Y_n)*F(Y_n)
-// for the vector-valued system F(Y) = 0.
-// Here, F(Y) is the nonlinear function and 
-// J(Y) = DF/DY its Jacobi matrix. The function
-// F(Y) is defined analogously to the right-hand
-// side in the LinSystem class, and the matrix
-// J(Y) analogously to the stiffness matrix.
+// 
+//  Nonlinear solver test:
+//
+//  PDE: stationary heat transfer with nonlinear thermal conductivity
+//  - div[lambda(T)grad T] = 0
+//
+//  Domain: square
+//
+//  BC:  T = 100 on the left, top and bottom edges
+//       dT/dn = 0 on the right edge
+//
 
-double RESIDUUM_NORM_TOL = 1e-5;  // The Newton's iteration stops if the norm of the 
-                                  // vector F(Y_{n+1}) is less than this value. 
-                                  // Currently, the maximum norm is used.
-double NEWTON_ALPHA = 1.0;        // Parameter for the Newton's method (0 < NEWTON_ALPHA <= 1)
-                                  // Default value is NEWTON_ALPHA = 1.
-double TEMP_BDY = 100.0;          // This temperature corresponds to the bdy marker 5,
-                                  // other bdy markers have zero Neumann condition
+/********** Problem parameters ***********/ 
+
+int PROJ_TYPE = 2;    // 1 for H1 projections, 2 for L2 projections
 
 // thermal conductivity (temperature-dependent)
-// NOTE: for any T, this function has to be  
-// positive in the entire domain!
-double lam(double T) { return 1 + pow(T,2); } 
-double dlam_dT(double T) { return 2*pow(T,1); }
-                                                                                                                      
-// definition of boundary conditions
-int T_bc_type(int marker)
-  { return (marker == 5) ? BC_ESSENTIAL : BC_NATURAL; }
-double T_bc_value(int marker, double x, double y)
-  { return TEMP_BDY; }
+// for any u, this function has to be  positive in the entire domain!
+double lam(double T) { return 1.0 + T*T; } 
+double dlam_dT(double T) { return 2*T; }
 
-// solution from the previous iteration
-Solution Tprev;
+/********** Definition of boundary conditions ***********/ 
 
-inline double J(RealFunction* Tprev, RealFunction* fu, 
+int bc_types(int marker)
+{
+ if (marker == 4 || marker == 1 || marker == 3) return BC_ESSENTIAL;
+//   if (marker == 4) return BC_ESSENTIAL;
+  else return BC_NATURAL;
+}
+
+scalar bc_values(int marker, double x, double y)
+{
+ if (marker == 4 || marker == 1 || marker == 3) return 100;
+//   if (marker == 4) return -4.0 * sqr(y) + 4.0 * y; 
+  else return 0.0;
+}
+
+
+/********** Definition of Jacobian matrices and residual vectors ***********/ 
+
+// Residuum 
+inline double F(RealFunction* Titer, RealFunction* fu, RefMap* ru)
+{
+  Quad2D* quad = fu->get_quad_2d();
+  RefMap* rv = ru;
+
+  int o = 3 * Titer->get_fn_order() + fu->get_fn_order() + ru->get_inv_ref_order();
+  limit_order(o);
+  Titer->set_quad_order(o);
+  fu->set_quad_order(o);
+
+  double* Titer_val = Titer->get_fn_values();
+  double* uval = fu->get_fn_values();
+  double *dTiter_dx, *dTiter_dy, *dudx, *dudy;
+  Titer->get_dx_dy_values(dTiter_dx, dTiter_dy);
+  fu->get_dx_dy_values(dudx, dudy);
+
+  // u is a test function
+  h1_integrate_dd_expression(( lam(Titer_val[i]) * (dTiter_dx[i]*t_dudx + dTiter_dy[i]*t_dudy)));
+
+  return result;
+}
+
+// Jacobian matrix 
+inline double J(RealFunction* Titer, RealFunction* fu, 
                 RealFunction* fv, RefMap* ru, RefMap* rv)
 {
   Quad2D* quad = fu->get_quad_2d();
 
-  int o = Tprev->get_fn_order() + fu->get_fn_order() 
-          + fv->get_fn_order() + ru->get_inv_ref_order() + 4;
+  int o = 2 * Titer->get_fn_order() + fu->get_fn_order() + fv->get_fn_order() + ru->get_inv_ref_order();
   limit_order(o);
-  Tprev->set_quad_order(o, FN_DEFAULT);
-  fu->set_quad_order(o, FN_DEFAULT);
-  fv->set_quad_order(o, FN_DEFAULT);
+  Titer->set_quad_order(o);
+  fu->set_quad_order(o);
+  fv->set_quad_order(o);
 
-  double* Tprev_val = Tprev->get_fn_values();
+  double* Titer_val = Titer->get_fn_values();
   double* uval = fu->get_fn_values();
   double* vval = fv->get_fn_values();
-  double *dTprev_dx, *dTprev_dy, *dudx, *dudy, *dvdx, *dvdy;
-  Tprev->get_dx_dy_values(dTprev_dx, dTprev_dy);
+
+  double *dTiter_dx, *dTiter_dy, *dudx, *dudy, *dvdx, *dvdy;
+  Titer->get_dx_dy_values(dTiter_dx, dTiter_dy);
   fu->get_dx_dy_values(dudx, dudy);
   fv->get_dx_dy_values(dvdx, dvdy);
 
-  double result0 = 0.0; 
-  double3* pt = quad->get_points(o); 
-  int np = quad->get_num_points(o); 
-  double2x2 *mv, *mu; 
-  mu = ru->get_inv_ref_map(o); 
-  mv = rv->get_inv_ref_map(o); 
-  double* jac = ru->get_jacobian(o); 
-  for (int i = 0; i < np; i++, mu++, mv++) {
-    //    result0 += pt[i][2] * jac[i] * (
-    //  dlam_dT(Tprev_val[i]) * vval[i] * (dTprev_dx[i]*t_dudx + dTprev_dy[i]*t_dudy)
-    //  + lam(Tprev_val[i]) * (t_dvdx*t_dudx + t_dvdy*t_dudy)); 
-    result0 += pt[i][2] * jac[i] * (
-      dlam_dT(Tprev_val[i]) * uval[i] * (dTprev_dx[i]*t_dvdx + dTprev_dy[i]*t_dvdy)
-      + lam(Tprev_val[i]) * (t_dudx*t_dvdx + t_dudy*t_dvdy)); 
-  }
+  // u is a basis function, v a test function
+  h1_integrate_dd_expression(( dlam_dT(Titer_val[i]) * uval[i] * (dTiter_dx[i]*t_dvdx + dTiter_dy[i]*t_dvdy) +
+                               lam(Titer_val[i]) * (t_dudx*t_dvdx + t_dudy*t_dvdy)));
 
-  return result0;
-}
-
-inline double F(RealFunction* Tprev, RealFunction* fu, RefMap* ru)
-{
-  Quad2D* quad = fu->get_quad_2d();
-
-  int o = Tprev->get_fn_order() + fu->get_fn_order() 
-          + ru->get_inv_ref_order() + 4;
-  limit_order(o);
-  Tprev->set_quad_order(o, FN_DEFAULT);
-  fu->set_quad_order(o, FN_DEFAULT);
-
-  double* Tprev_val = Tprev->get_fn_values();
-  double* vval = fu->get_fn_values();
-  double *dTprev_dx, *dTprev_dy, *dudx, *dudy;
-  Tprev->get_dx_dy_values(dTprev_dx, dTprev_dy);
-  fu->get_dx_dy_values(dudx, dudy);
-
-  double result = 0.0; 
-  double3* pt = quad->get_points(o); 
-  int np = quad->get_num_points(o); 
-  double2x2 *mv, *mu; 
-  mu = ru->get_inv_ref_map(o); 
-  // mv = rv->get_inv_ref_map(o); 
-  double* jac = ru->get_jacobian(o); 
-  for (int i = 0; i < np; i++, mu++, mv++) {
-    result += pt[i][2] * jac[i] * (
-      lam(Tprev_val[i]) * (dTprev_dx[i]*t_dudx + dTprev_dy[i]*t_dudy)); 
-  }
   return result;
 }
 
-scalar bilinear_form_sym_0_0(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
-{ return J(&Tprev, fu, fv, ru, rv); }
+/********** Definition of linear and bilinear forms for Hermes ***********/ 
+
+Solution Titer; // solution converging during the Newton's iteration
 
 scalar linear_form_0(RealFunction* fv, RefMap* rv)
-{ return F(&Tprev, fv, rv); }
+{ return F(&Titer, fv, rv); }
+scalar bilinear_form_0_0(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
+{ return J(&Titer, fu, fv, ru, rv); }
+
+// *************************************************************
 
 int main(int argc, char* argv[])
 {
-  // load the mesh file
   Mesh mesh;
-  // mesh.load("hole_new.mesh");
   mesh.load("square.mesh");
-  mesh.refine_all_elements();
-  mesh.refine_all_elements();
-  mesh.refine_all_elements();
-
-  // mesh.refine_towards_boundary(5, 0);
-
-  H1ShapesetBeuchler shapeset;
+  for(int i = 0; i < 5; i++) mesh.refine_all_elements();
+  
+  H1Shapeset shapeset;
   PrecalcShapeset pss(&shapeset);
+  
+  H1Space space(&mesh, &shapeset);
+  space.set_bc_types(bc_types);
+  space.set_bc_values(bc_values);
+  space.set_uniform_order(1);
+  space.assign_dofs();
 
-  // space for the temperature
-  H1Space T(&mesh, &shapeset);
-
-  // initialize boundary conditions
-  T.set_bc_types(T_bc_type);
-  T.set_bc_values(T_bc_value);
-
-  // set element polynomial degrees
-  T.set_uniform_order(1);
-
-  // assign degrees of freedom and calculate problem size
-  int ndofs = 0;
-  ndofs += T.assign_dofs(ndofs);
-
-  // setting zero initial condition
-  // Tprev.set_zero(&mesh);
-  scalar *y_temp = new scalar[ndofs];
-  for (int i = 0; i<ndofs; i++) y_temp[i] = 0;
-  Tprev.set_fe_solution(&T, &pss, y_temp);
-  delete [] y_temp;
-
-  // visualization
-  ScalarView Tview("Temperature", 0, 500, 1200, 470);
-  // Tview.set_min_max_range(0, TEMP_BDY);
-  // Fview.show_scale(false);
-
-  // set up weak formulation
   WeakForm wf(1);
-  wf.add_biform(0, 0, bilinear_form_sym_0_0, SYM, 0, 1, &Tprev);
-  wf.add_liform(0, linear_form_0, 0, 1, &Tprev);
+  wf.add_biform(0, 0, bilinear_form_0_0, UNSYM, ANY, 1, &Titer);
+  wf.add_liform(0, linear_form_0, ANY, 1, &Titer);
 
-  // set up the nonlinear system
   UmfpackSolver umfpack;
-  NonlinSystem nsys(&wf, &umfpack);
-  nsys.set_spaces(1, &T);
-  nsys.set_pss(1, &pss);
-  nsys.set_alpha(NEWTON_ALPHA);
+  NonlinSystem nls(&wf, &umfpack);
+  nls.set_spaces(1, &space);
+  nls.set_pss(1, &pss);
 
-  // defining zero initial condition for the Newton's 
-  // iteration (fixme - Tprev.set_zero(&mesh) used above 
-  // should be enough to do this.
-  nsys.set_vec_zero();
+  char title[100];  
+  ScalarView view("", 0, 0, 600, 600);
+  ScalarView view2("", 700, 0, 600, 600);
+  
+  // setting initial condition for the Newton's method
+  Titer.set_const(&mesh, 0.0);
+  nls.set_ic(&Titer, &Titer, PROJ_TYPE);
 
-  // main loop
-  int count = 0;
-  double residuum_norm = 1e10;
-  do 
+  // view initial guess for Newton's method 
+  sprintf(title, "Initial guess for the Newton's method");
+  view.set_title(title);
+  view.show(&Titer);    
+  view.wait_for_keypress();
+
+  Solution sln;
+    
+  int it = 1; 
+  double res_l2_norm; 
+  do
   {
-    Solution Tsln;
-    info("\n*** Iteration %d ***", ++count);
+    info("\n---- Newton iter %d ---------------------------------\n", it++);
     
-    // assemble
-    nsys.assemble();
+    nls.assemble();
+    nls.solve(1, &sln);
 
-    /* DEBUG
-    // output matrix and RHS in Matlab format
-    char *filename = new char[20];
-    strcpy(filename, "matrix.txt");
-    char *varname = new char[20];
-    strcpy(varname, "matrix");
-    nsys.save_matrix_matlab(filename, varname);
-    strcpy(filename, "rhs.txt");
-    strcpy(varname, "rhs");
-    nsys.save_rhs_matlab(filename, varname);
-    */
- 
-    // solve
-    nsys.solve(1, &Tsln);
+    res_l2_norm = nls.get_residuum_l2_norm(); 
+    info("Residuum L2 norm: %g\n", res_l2_norm);
+    // want to see Newtons iterations       
+    sprintf(title, "Newton iteration %d", it-1);
+    view.set_title(title);
+    view.show(&sln);    
+    view.wait_for_keypress();
 
-    // calculate the residuum
-    residuum_norm = nsys.get_residuum_max_norm();
-    info("Residuum max norm = %g\n", residuum_norm);
-    
-    // visualization of intermediate Newton's iterations
-    //Tview.show(&Tsln, 2*EPS_LOW);    
-    //Tview.wait();
-
-    // uncomment one of the following lines to generate a series of video frames
-    // vview.save_numbered_screenshot("velocity%03d.bmp", i, true);
-    // pview.save_numbered_screenshot("pressure%03d.bmp", i, true);
-    // the frames can then be converted to a video file with the command
-    // mencoder "mf://velocity*.bmp" -mf fps=20 -o velocity.avi -ovc lavc -lavcopts vcodec=mpeg4
-    
-    Tprev = Tsln;
-
-    info("Residuum norm = %g\n", residuum_norm);
-  } while(residuum_norm > RESIDUUM_NORM_TOL);
+    Titer = sln;
+       
+  }
+  while (res_l2_norm > 1e-4);
 
   View::wait();
+  return 0;
 }
+
