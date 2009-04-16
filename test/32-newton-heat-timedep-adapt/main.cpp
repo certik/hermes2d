d48e01b29189335efa12874778fcbc077b7b4569
svn <svn@dbed12e5-3d52-4894-8360-3e909281c254>
2009-04-16 13:38:09 +0000

    New test 32 for dynamical hp-meshes for a nonlinear time-dependent heat transfer equation.
    git-svn-id: svn+ssh://spilka.math.unr.edu/hermes2d/trunk@202 dbed12e5-3d52-4894-8360-3e909281c254

--- test/32-newton-heat-timedep-adapt/main.cpp
+++ test/32-newton-heat-timedep-adapt/main.cpp
@@ -1,29 +1,29 @@
 #include "hermes2d.h"
 #include "solver_umfpack.h"
 
+// 
+//  Nonlinear solver test:
+//
+//  hp-adaptivity with dynamical meshes
 //
 //  PDE: non-stationary heat transfer with nonlinear thermal conductivity
 //  HEATCAP*dT/dt - div[lambda(T)grad T] = 0
-// 
-//  Solved using dynamical meshes
 //
 //  Domain: square
 //
 //  BC:  T = 100 on the left, top and bottom edges
 //       dT/dn = 0 on the right edge
 //
-//  Time-stepping: either implicit Euler or Crank-Nicolson
-//
 
 /********** Problem parameters ***********/ 
 
-int TIME_DISCR = 2;   // 1 for implicit Euler, 2 for Crank-Nicolson
+int TIME_DISCR = 1;   // 1 for implicit Euler, 2 for Crank-Nicolson
 int PROJ_TYPE = 1;    // 1 for H1 projections, 0 for L2 projections
 double HEATCAP = 1e6; // heat capacity
 double TAU = 0.5;     // time step
-int NSTEP = 100;      // number of time steps to do
-int SUBDIV_INIT = 2;  // number of levels of initial uniform refinement
-int P_INIT = 2;       // initial polynomial degree
+double T_FINAL = 60;  // time interval length
+int P_INIT = 2;       // initial uniform polynomial order
+int REF_INIT = 2;     // number of initial refinements
 
 // thermal conductivity (temperature-dependent
 // for any u, this function has to be  positive in the entire domain!
@@ -46,207 +46,170 @@ scalar bc_values(int marker, double x, double y)
   else return 0.0;
 }
 
-/********** Import of Jacobian matrices and residual vectors ***********/ 
+/********** Definition of Jacobian matrices and residual vectors ***********/ 
 
 # include "forms.cpp"
 
-/********** Definition of linear and bilinear forms for Hermes *********/ 
+/********** Definition of linear and bilinear forms for Hermes ***********/ 
 
-Solution Tprev_bas,      // previous time step solution on basic mesh
-         Tprev_crs,      // previous time step solution on coarse mesh
-         Tprev_ref,      // previous time step solution on reference mesh
-         Titer_bas,      // iterated solution on basic mesh
-         Titer_crs,      // iterated solution on coarse mesh 
-         Titer_ref;      // iterated solution on reference mesh 
+Solution Tprev, // previous time step solution, for the time integration method
+         Titer; // solution converging during the Newton's iteration
 
 // Implicit Euler method (1st-order in time)
 scalar linear_form_0_euler(RealFunction* fv, RefMap* rv)
-{ return F_euler(&Tprev_bas, &Titer_bas, fv, rv); }
+{ return F_euler(&Tprev, &Titer, fv, rv); }
 scalar bilinear_form_0_0_euler(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
-{ return J_euler(&Titer_bas, fu, fv, ru, rv); }
+{ return J_euler(&Titer, fu, fv, ru, rv); }
 
 // Crank-Nicolson method (2nd-order in time)
 scalar linear_form_0_cranic(RealFunction* fv, RefMap* rv)
-{ return F_cranic(&Tprev_bas, &Titer_bas, fv, rv); }
+{ return F_cranic(&Tprev, &Titer, fv, rv); }
 scalar bilinear_form_0_0_cranic(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
-{ return J_cranic(&Titer_bas, fu, fv, ru, rv); }
+{ return J_cranic(&Titer, fu, fv, ru, rv); }
 
-/********** Main program ***********************************************/ 
+// *************************************************************
 
 int main(int argc, char* argv[])
 {
-  // meshes
-  Mesh 
-    mesh_bas,   // basic 
-    mesh_ref,   // reference refined
-    mesh_crs;   // reference coarsened
-  mesh_bas.load("square.mesh");
-  for(int i = 0; i < SUBDIV_INIT; i++) mesh_bas.refine_all_elements();
-
-  // precalculated shapeset
+  Mesh mesh, basemesh;
+  basemesh.load("square.mesh");
+  for(int i = 0; i < REF_INIT; i++) basemesh.refine_all_elements();
+  mesh.copy(&basemesh);
+  
   H1Shapeset shapeset;
   PrecalcShapeset pss(&shapeset);
-
-  // registration of weak forms
-  WeakForm wf(1);
-  if(TIME_DISCR == 1) {
-    wf.add_biform(0, 0, bilinear_form_0_0_euler, UNSYM, ANY, 1, &Titer_bas);
-    wf.add_liform(0, linear_form_0_euler, ANY, 2, &Titer_bas, &Tprev_bas);
-  }
-  else {
-    wf.add_biform(0, 0, bilinear_form_0_0_cranic, UNSYM, ANY, 1, &Titer_bas);
-    wf.add_liform(0, linear_form_0_cranic, ANY, 2, &Titer_bas, &Tprev_bas);
-  }
   
-  // space for the solution
-  H1Space space(&mesh_bas, &shapeset);
+  H1Space space(&mesh, &shapeset);
   space.set_bc_types(bc_types);
   space.set_bc_values(bc_values);
-  space.set_uniform_order(P_INIT);
-  space.assign_dofs();
+  space.set_uniform_order(2);
+  space.assign_dofs();    
 
-  // matrix solver
+  WeakForm wf(1);
+  if(TIME_DISCR == 1) {
+    wf.add_biform(0, 0, bilinear_form_0_0_euler, UNSYM, ANY, 1, &Titer);
+    wf.add_liform(0, linear_form_0_euler, ANY, 2, &Titer, &Tprev);
+  }
+  else {
+    wf.add_biform(0, 0, bilinear_form_0_0_cranic, UNSYM, ANY, 1, &Titer);
+    wf.add_liform(0, linear_form_0_cranic, ANY, 2, &Titer, &Tprev);
+  }
+  
   UmfpackSolver umfpack;
-
-  // nonlinear system
   NonlinSystem nls(&wf, &umfpack);
+  nls.set_spaces(1, &space);
   nls.set_pss(1, &pss);
 
-  // visualization
   char title[100];  
   ScalarView view("", 0, 0, 600, 600);
-
-  // setting the Dirichlet lift to be the 
-  // initial condition on thebasic mesh
-  nls.set_spaces(1, &space);  
-  Tprev_bas.set_dirichlet_lift(&space, &pss);
-  nls.set_ic(&Tprev_bas, &Tprev_bas, PROJ_TYPE);
-  Titer_bas.copy(&Tprev_bas);
-
-  // view initial condition
-  /*
-  sprintf(title, "Initial condition");
-  view.set_title(title);
-  view.show(&Titer_bas);    
-  view.wait_for_keypress();
-  */  
+  OrderView ordview("Polynomial Orders", 0, 700, 600, 600);
   
+  GnuplotGraph graph_err;
+  graph_err.set_captions("","Time step","Error");
+  graph_err.add_row();
+  GnuplotGraph graph_dofs;
+  graph_dofs.set_captions("","Time step","DOFs");
+  graph_dofs.add_row();
+  
+  // initial condition at zero time level
+  Tprev.set_const(&mesh, 0.0);
+  nls.set_ic(&Tprev, &Tprev);
+  Titer.copy(&Tprev);
+
+  // view initial guess for Newton's method
+  // satisfies BC conditions 
+  sprintf(title, "Initial iteration");
+  view.set_title(title);
+  view.show(&Titer);    
+  ordview.show(&space);
+  //view.wait_for_keypress(); // this may cause graphics problems
+
+  Solution sln, rsln;
   // time stepping
-  Solution sln, sln_ref, sln_crs;
-  for(int n = 1; n <= NSTEP; n++) 
+  int nstep = (int)(T_FINAL/TAU + 0.5);
+  for(int n = 1; n <= nstep; n++) 
   {
 
-    info("\n---- Time step %d -----------------------------------------------", n);
-
-    // ***** COARSE SOLUTION *****
-    // unrefine mesh_bas to mesh_crs, 
-    // project solution from mesh_bas to mesh_crs, 
-    // solve problem on mesh_crs
-    mesh_crs.copy(&mesh_bas);  
-    mesh_crs.unrefine_all_elements();
-    space.set_mesh(&mesh_crs); 
-    space.assign_dofs();
-    nls.set_spaces(1, &space);  
-    Tprev_crs.copy(&Tprev_bas); 
-    nls.set_ic(&Tprev_crs, &Tprev_crs, PROJ_TYPE);
-    Titer_crs.copy(&Tprev_crs);
-    int it = 1; 
-    double res_l2_norm; 
-    do
-    {
-      info("\n---- Solution (coarse): tstep %d, iter %d ---------------------------------\n", n, it++);
-      nls.assemble();
-      nls.solve(1, &sln);
-      res_l2_norm = nls.get_residuum_l2_norm(); 
-      info("Residuum L2 norm: %g\n", res_l2_norm);
-      Titer_crs = sln;
+    info("\n---- Time step %d -----------------------------------------------------------------", n);
+    
+    // unrefinements
+    if (n % 10 == 2) {
+      mesh.copy(&basemesh);
+      space.set_uniform_order(2);
     }
-    while (res_l2_norm > 1e-4);
-    Tprev_crs.copy(&Titer_crs);
-
-    // visualization of coarse solution 
-    //sprintf(title, "Solution (coarse): Time level %d", n);
-    //view.set_title(title);
-    //view.show(&Titer_crs);    
-    //view.wait_for_keypress();
-
-    // ***** BASIC SOLUTION *****
-    // project solution from mesh_crs to mesh_bas, 
-    // solve problem on mesh_bas
-    space.set_mesh(&mesh_bas); 
-    space.assign_dofs();
-    nls.set_spaces(1, &space);  
-    Tprev_bas.copy(&Tprev_crs); 
-    nls.set_ic(&Tprev_bas, &Tprev_bas, PROJ_TYPE);
-    it = 1; 
-    space.assign_dofs();
-    do
+    
+    int at = 0;
+    bool done = false;
+    double err;
+    do 
     {
-      info("\n---- Solution (basic): tstep %d, iter %d ---------------------------------\n", n, it++);
-      nls.assemble();
-      nls.solve(1, &sln);
-      res_l2_norm = nls.get_residuum_l2_norm(); 
-      info("Residuum L2 norm: %g\n", res_l2_norm);
-      Titer_bas = sln;
-    }
-    while (res_l2_norm > 1e-4);
-    Tprev_bas.copy(&Titer_bas);
-
-    // visualization of solution on the n-th time level
-    //sprintf(title, "Solution: Time level %d", n);
-    //view.set_title(title);
-    //view.show(&Titer);    
-    //view.wait_for_keypress();
-
-    // ***** REFERENCE SOLUTION *****
-    // refine mesh_bas to mesh_ref, 
-    // project solution from mesh_bas to mesh_ref, 
-    // solve problem on mesh_ref
-    mesh_ref.copy(&mesh_bas);  
-    mesh_ref.refine_all_elements();
-    space.set_mesh(&mesh_ref); 
-    space.assign_dofs();
-    nls.set_spaces(1, &space);  
-    Tprev_ref.copy(&Tprev_bas); 
-    nls.set_ic(&Tprev_ref, &Tprev_ref, PROJ_TYPE);
-    Titer_ref.copy(&Tprev_ref);
-    it = 1; 
-    do
-    {
-      info("\n---- Solution (fine): tstep %d, iter %d ---------------------------------\n", n, it++);
-      nls.assemble();
-      nls.solve(1, &sln);
-      res_l2_norm = nls.get_residuum_l2_norm(); 
-      info("Residuum L2 norm: %g\n", res_l2_norm);
-      Titer_crs = sln;
-    }
-    while (res_l2_norm > 1e-4);
-    Tprev_ref.copy(&Titer_ref);
-
-    // visualization of fine solution 
-    //sprintf(title, "Solution (fine): Time level %d", n);
-    //view.set_title(title);
-    //view.show(&Titer_ref);    
-    //view.wait_for_keypress();
+     info("\n---- Time step %d, adaptivity step %d ---------------------------------------------\n", n, ++at);
 
     
+      int it = 1; 
+      double res_l2_norm; 
+      space.assign_dofs();    
+      if (n > 1 || at > 1) nls.set_ic(&rsln, &Titer);
+      else nls.set_ic(&Titer, &Titer);
+      do
+      {
+        info("\n---- Time step %d, adaptivity step %d, Newton step %d -----------------------------\n", n, at, it++);
+        
+        nls.assemble();
+        nls.solve(1, &sln);
+  
+        res_l2_norm = nls.get_residuum_l2_norm(); 
+        info("Residuum L2 norm: %g\n", res_l2_norm);
+   
+        Titer.copy(&sln);          
+      }
+      while (res_l2_norm > 1e-4);
+            
+      // reference solution
+      it = 1;
+      RefNonlinSystem rs(&nls);
+      rs.prepare();
+      if (n > 1 || at > 1) rs.set_ic(&rsln, &Titer);
+      else rs.set_ic(&Titer, &Titer);
+      do
+      {
+        info("\n---- Time step %d, adaptivity step %d, Newton step %d (Reference solution) --------\n", n, at, it++);
+        
+        rs.assemble();
+        rs.solve(1, &rsln);
 
-    // TODO - ADAPTIVITY 
+        res_l2_norm = rs.get_residuum_l2_norm(); 
+        info("Residuum L2 norm: %g\n", res_l2_norm);
+  
+        Titer.copy(&rsln);          
+      }
+      while (res_l2_norm > 1e-4);
 
+      H1OrthoHP hp(1, &space);
+      err = hp.calc_error(&sln, &rsln) * 100;
+      info("Error: %g", err);
+      if (err < 0.001) done = true;
+      else hp.adapt(0.3, 1);
 
+    }
+    while (!done);
+      
+    // visualization of solution on the n-th time level
+    sprintf(title, "Time level %d", n);
+    view.set_min_max_range(90,100);
+    view.set_title(title);
+    view.show(&Titer);    
+    ordview.show(&space);
+    //view.wait_for_keypress();
 
-
-
-
-
-
-
-
-
-
-
-
-  } // end of time stepping loop
+    graph_err.add_values(0, n, err);
+    graph_err.save("error.txt");
+    graph_dofs.add_values(0, n, space.get_num_dofs());
+    graph_dofs.save("dofs.txt");
+    
+    // copying result of the Newton's iteration into Tprev
+    Tprev.copy(&Titer);
+  }  
 
   View::wait();
   return 0;
