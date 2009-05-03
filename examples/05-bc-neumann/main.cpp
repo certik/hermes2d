45dcb9049709a20b465aba01343c8d5786c064b2
svn <svn@dbed12e5-3d52-4894-8360-3e909281c254>
2009-05-03 05:45:45 +0000

    Example 05 improved.
    git-svn-id: svn+ssh://spilka.math.unr.edu/hermes2d/trunk@241 dbed12e5-3d52-4894-8360-3e909281c254

--- examples/05-bc-neumann/main.cpp
+++ examples/05-bc-neumann/main.cpp
@@ -1,17 +1,32 @@
 #include "hermes2d.h"
 #include "solver_umfpack.h"
 
+// This example is a continuation of examples 03 and 04. It shows 
+// you how to use Neumann boundary conditions.
+//
+// PDE: Poisson equation -Laplace u = f, where f = -1 
+//
+// The exact solution to this problem has a singular (infinite) 
+// gradient at the re-entrant corner. You can play with the 
+// initial polynomial degree P_INIT, with the number of refinements 
+// towards the re-entrant corner, etc. 
+
+int P_INIT = 2;             // initial polynomial degree in all elements
+int CORNER_REF_LEVEL = 10;  // number of mesh refinements towards the re-entrant corner
 
 int bc_types(int marker)
 {
-  return (marker == 4) ? BC_ESSENTIAL : BC_NATURAL;
+  // Note: essential means Dirichlet (prescribed is value of solution at the boundary). 
+  // Natural means Neumann, Newton, or any other type of condition where the solution 
+  // value is not prescribed.
+  return (marker == 4) ? BC_ESSENTIAL : BC_NATURAL; 
 }
 
 scalar bc_values(int marker, double x, double y)
 {
-  return 0.0; // Dirichlet BC value
+  if (marker == 4) return 0.0; // Dirichlet BC value
+  else return 0.0;             // Neumann BC value
 }
-
 
 scalar bilinear_form(RealFunction* fu, RealFunction* fv, RefMap* ru, RefMap* rv)
 {
@@ -34,6 +49,7 @@ int main(int argc, char* argv[])
   // load the mesh file
   Mesh mesh;
   mesh.load("domain.mesh");
+  mesh.refine_towards_vertex(3, CORNER_REF_LEVEL);
 
   // initialize the shapeset and the cache
   H1Shapeset shapeset;
@@ -43,7 +59,7 @@ int main(int argc, char* argv[])
   H1Space space(&mesh, &shapeset);
   space.set_bc_types(bc_types);
   space.set_bc_values(bc_values);
-  space.set_uniform_order(5);
+  space.set_uniform_order(P_INIT);
   space.assign_dofs();
 
   // initialize the weak formulation
@@ -63,10 +79,16 @@ int main(int argc, char* argv[])
   sys.assemble();
   sys.solve(1, &sln);
 
-  // visualize the solution
-  ScalarView view("Solution");
+  // visualize the approximation
+  ScalarView view("Solution", 0, 0, 600, 600);
   view.show(&sln);
-  
+
+  // TODO: show gradient magnitude
+
+
+
+ 
+  printf("Waiting for keyboard or mouse input.\n");
   View::wait();
   return 0;
 }
