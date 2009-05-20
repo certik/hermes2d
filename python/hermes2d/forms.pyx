7614f5229918b9f15cb979cd7bcdb9e9983e10a7
Ondrej Certik <ondrej@certik.cz>
2009-05-19 17:51:56 -0700

    forms.pyx: the "void *data" parameter simulated in Cython
    That's because this is not yet implemented in the C++ hermes2d. So we just
    hardwire several special cases, that are needed in tests.
    
    The real fix should be to implmenet this in hermes C++, but that will require
    some substantial changes to the assembling procedure, so this should be done
    later, when all the Python stuff works fine.
    
    Also the example 08 was fixed.

--- python/hermes2d/forms.pyx
+++ python/hermes2d/forms.pyx
@@ -1,21 +1,26 @@
 from hermes2d.hermes2d cimport scalar, RealFunction, RefMap, WeakForm, \
         int_grad_u_grad_v, int_v, malloc
 
-cdef struct Data:
-    int k
-
 cdef scalar bilinear_form(RealFunction *fu, RealFunction *fv,
         RefMap *ru, RefMap *rv):
     return int_grad_u_grad_v(fu, fv, ru, rv)
 
-cdef scalar linear_form(RealFunction *fv, RefMap *rv, void *data):
-    cdef Data *d = <Data *>data
-    return d.k * int_v(fv, rv)
+cdef scalar linear_form_p2(RealFunction *fv, RefMap *rv):
+    return 2*int_v(fv, rv)
+
+cdef scalar linear_form_m1(RealFunction *fv, RefMap *rv):
+    return -int_v(fv, rv)
+
+cdef scalar linear_form_m4(RealFunction *fv, RefMap *rv):
+    return -4*int_v(fv, rv)
 
 def set_forms(WeakForm wf, int k=2):
     wf.thisptr.add_biform(0, 0, &bilinear_form)
-    wf.thisptr.add_liform(0, &linear_form)
-    cdef Data *data
-    data = <Data *>malloc(sizeof(Data))
-    data.k = k
-    wf.thisptr.add_liform_data(0, data)
+    if k == 2:
+        wf.thisptr.add_liform(0, &linear_form_p2)
+    elif k == -1:
+        wf.thisptr.add_liform(0, &linear_form_m1)
+    elif k == -4:
+        wf.thisptr.add_liform(0, &linear_form_m4)
+    else:
+        raise NotImplementedError()
