4485aa928365e3acbc4d1fc8be39c0d3ce1078c1
Ondrej Certik <ondrej@certik.cz>
2009-05-31 15:20:56 -0600

    white space stripped in util/linview
    Signed-off-by: David Andrs <andrsd@gmail.com>

--- util/linview/main.cpp
+++ util/linview/main.cpp
@@ -8,21 +8,21 @@ template<class Base>
 class LinView : public Base
 {
 public:
-  
-  LinView(int width, int height, int n, char** names) 
+
+  LinView(int width, int height, int n, char** names)
     : current(0), num(n), names(names),
       Base("LinView", 0, 0, width, height) {}
-  
+
   void switch_to(int n)
   {
     Base::load_data(names[n]);
     update_title();
   }
-  
+
   int get_num() const { return num; }
-    
+
 protected:
-  
+
   int num, current;
   char** names;
 
@@ -33,7 +33,7 @@ protected:
       case GLUT_KEY_PAGE_UP:
         if (current > 0) switch_to(--current);
         break;
-        
+
       case GLUT_KEY_PAGE_DOWN:
         if (current < num-1) switch_to(++current);
         break;
@@ -45,11 +45,11 @@ protected:
       case GLUT_KEY_END:
         switch_to(current = num-1);
         break;
-        
+
       default: Base::on_special_key(key, x, y);
     }
   }
-  
+
   void update_title()
   {
     char buffer[1000];
@@ -97,7 +97,7 @@ int main(int argc, char* argv[])
 {
   verbose_mode = false;
 
-  struct 
+  struct
   {
     int width, height, palsteps;
     double consteps, conorig;
@@ -107,7 +107,7 @@ int main(int argc, char* argv[])
     char scaleformat[50];
   }
   options;
-  
+
   options.width = 1000;
   options.height = 750;
   options.scale = false;
@@ -122,7 +122,7 @@ int main(int argc, char* argv[])
   options.conorig = 0.0;
   strcpy(options.filename, "frame%04d.bmp");
   strcpy(options.scaleformat, "%.3g");
-  
+
   static const option long_opts[] =
   {
     { "width",        1, NULL, 'w' },
@@ -141,7 +141,7 @@ int main(int argc, char* argv[])
     { "cont-orig",     1, NULL, 'o' },
     { NULL,           0, NULL,  0  }
   };
-  
+
   int opt, long_idx;
   while ((opt = getopt_long(argc, argv, "w:h:m:M:svcqf:t:p:k:o:?", long_opts, &long_idx)) != -1)
   {
@@ -163,15 +163,15 @@ int main(int argc, char* argv[])
       default:  print_help(); return EXIT_FAILURE;
     }
   }
-  
-  if (optind >= argc) 
+
+  if (optind >= argc)
   {
     printf("No files to process. To get help, run with --help\n");
     return EXIT_FAILURE;
   }
-  
+
   LinView<WHATVIEW> linview(options.width, options.height, argc-optind, argv+optind);
-  
+
   linview.show_scale(options.scale);
   linview.set_scale_format(options.scaleformat);
   if (options.min != options.max)
@@ -194,7 +194,7 @@ int main(int argc, char* argv[])
       char filename[100];
       sprintf(filename, options.filename, i+1);
       if (!exists(filename))
-      { 
+      {
         linview.switch_to(i);
         usleep(100000); // bad intel GL driver
         linview.save_screenshot(filename, options.hq);
@@ -203,7 +203,7 @@ int main(int argc, char* argv[])
         info("%s already exists: skipping", filename);
     }
   }
-  
+
   if (not options.just_convert)
       View::wait();
   return EXIT_SUCCESS;
