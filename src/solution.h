4f3193e310388541229d239e0980567786828e87
Lenka Dubcova <dubcova@gmail.com>
2009-06-02 11:57:25 +0200

    Transformation of solution values (H1, Hcurl, Hdiv)
    Signed-off-by: David Andrs <andrsd@gmail.com>

--- src/solution.h
+++ src/solution.h
@@ -172,6 +172,7 @@ protected:
   int num_dofs;
 
   int space_type;
+  void transform_values(int order, Node* node, int newmask, int oldmask, int np);
 
   scalar   (*exactfn1)(double x, double y, scalar& dx,  scalar& dy);
   scalar2& (*exactfn2)(double x, double y, scalar2& dx, scalar2& dy);
