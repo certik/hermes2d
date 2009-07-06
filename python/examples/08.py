e2f8c62de7c25a4beec74f190178bece59665b4b
Ondrej Certik <ondrej@certik.cz>
2009-07-05 22:44:16 -0600

    examples: use get_example_mesh() to import the mesh
    This makes it more robust, because get_example_mesh() always knows where the
    domain.mesh is (it can be installed in some directory). Previously it just
    opened the file "domain.mesh" in the current directory, which failed if the
    current directory didn't have the file.
    
    Signed-off-by: David Andrs <andrsd@gmail.com>

--- python/examples/08.py
+++ python/examples/08.py
@@ -4,9 +4,10 @@ from hermes2d import Mesh, H1Shapeset, PrecalcShapeset, H1Space, \
         LinSystem, WeakForm, DummySolver, Solution, ScalarView, VectorView
 
 from hermes2d.examples.c08 import set_bc, set_forms
+from hermes2d.examples import get_cylinder_mesh
 
 mesh = Mesh()
-mesh.load("cylinder4.mesh")
+mesh.load(get_cylinder_mesh())
 #mesh.refine_element(0)
 #mesh.refine_all_elements()
 mesh.refine_towards_boundary(5, 3)
