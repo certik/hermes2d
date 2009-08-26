514027fa92b174be0fb3a7dbecb1b9886d75032a
Lenka Dubcova <dubcova@gmail.com>
2009-08-26 13:14:17 -0700

    safe_max_order instead of max_order in limit_order (so that triangles works)
    Signed-off-by: David Andrs <andrsd@gmail.com>
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

--- src/linsystem.h
+++ src/linsystem.h
@@ -184,22 +184,23 @@ protected:
 extern void set_order_limit_table(int* tri_table, int* quad_table, int n);
 
 // limit_order is used in integrals
+extern int  g_safe_max_order;
 extern int  g_max_order;
 extern int* g_order_table;
 
 #ifndef DEBUG_ORDER
   #define limit_order(o) \
-    if (o > g_max_order) { o = g_max_order; warn_order(); } \
+    if (o > g_safe_max_order) { o = g_safe_max_order; warn_order(); } \
     o = g_order_table[o];
   #define limit_order_nowarn(o) \
-    if (o > g_max_order) o = g_max_order; \
+    if (o > g_safe_max_order) o = g_safe_max_order; \
     o = g_order_table[o];
 #else
   #define limit_order(o) \
     if (o > g_max_order) warn_order(); \
-    o = g_max_order;
+    o = g_safe_max_order;
   #define limit_order_nowarn(o) \
-    o = g_max_order;
+    o = g_safe_max_order;
 #endif
 
 extern void warn_order();
