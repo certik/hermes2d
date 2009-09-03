f31662d5f93b052609933906769a9457b2f3d9fa
Lenka Dubcova <dubcova@gmail.com>
2009-09-03 15:53:28 -0700

    bug fix in exact_sol.cpp (screen problem)
    Signed-off-by: David Andrs <andrsd@gmail.com>
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>

--- examples/screen/exact_sol.cpp
+++ examples/screen/exact_sol.cpp
@@ -182,7 +182,7 @@ static void exact_sol(double x, double y, scalar& u0, scalar& u1, scalar& u1dx, 
 
 scalar2& exact(double x, double y, scalar2& dx, scalar2& dy)
 {
-  scalar2 ex;
+  static scalar2 ex;
   exact_sol(x,y, ex[0], ex[1], dx[1], dy[0]);
 
   dx[0] = 0.0; // not important
