af3edb1e5679cbd5d7c9bc1146cee69540f1dfa5
Ondrej Certik <ondrej@certik.cz>
2009-10-25 15:09:56 -0700

    Other examples fixed
    Signed-off-by: Pavel Solin <solin@unr.edu>

--- python/hermes2d/examples/c05.pyx
+++ python/hermes2d/examples/c05.pyx
@@ -1,6 +1,7 @@
 from hermes2d._hermes2d cimport scalar, WeakForm, H1Space, EdgePos, \
         FuncReal, GeomReal, ExtDataReal, BC_ESSENTIAL, \
-        BC_NATURAL, int_v, c_Ord, create_Ord, FuncOrd, GeomOrd, ExtDataOrd
+        BC_NATURAL, int_v, c_Ord, create_Ord, FuncOrd, GeomOrd, ExtDataOrd, \
+        int_v_ord
 
 CONST_GAMMA = [-0.5, 1.0, -0.5]
 
@@ -19,9 +20,7 @@ cdef scalar bc_values_05(int marker, double x, double y):
 
 cdef c_Ord _order_lf(int n, double *wt, FuncOrd *u, GeomOrd
         *e, ExtDataOrd *ext):
-    # XXX: with 9 it doesn't shout about the integration order, but gives wrong
-    # results...
-    return create_Ord(20)
+    return int_v_ord(n, wt, u).mul_double(CONST_GAMMA[e.marker-1])
 
 def set_forms(WeakForm dp):
     dp.thisptr.add_liform_surf(0, &linear_form_surf_05, &_order_lf);
--- python/hermes2d/forms.pyx
+++ python/hermes2d/forms.pyx
@@ -1,6 +1,6 @@
 from hermes2d._hermes2d cimport scalar, FuncReal, GeomReal, WeakForm, \
-        int_grad_u_grad_v, int_grad_u_grad_v_ord, int_v, malloc, ExtDataReal, c_Ord, create_Ord, \
-        FuncOrd, GeomOrd, ExtDataOrd
+        int_grad_u_grad_v, int_grad_u_grad_v_ord, int_v, malloc, ExtDataReal, \
+        c_Ord, create_Ord, FuncOrd, GeomOrd, ExtDataOrd, int_v_ord
 
 cdef scalar bilinear_form(int n, double *wt, FuncReal *u, FuncReal *v, GeomReal
         *e, ExtDataReal *ext):
@@ -8,7 +8,10 @@ cdef scalar bilinear_form(int n, double *wt, FuncReal *u, FuncReal *v, GeomReal
 
 cdef c_Ord bilinear_form_ord(int n, double *wt, FuncOrd *u, FuncOrd *v, GeomOrd
         *e, ExtDataOrd *ext):
-    return int_grad_u_grad_v_ord(n, wt, u, v)
+    # surprisingly, uncommenting this makes it a lot slower:
+    #print int_grad_u_grad_v_ord(n, wt, u, v).get_order(),
+    #return int_grad_u_grad_v_ord(n, wt, u, v)
+    return create_Ord(20)
 
 cdef scalar linear_form_p2(int n, double *wt, FuncReal *u, GeomReal
         *e, ExtDataReal *ext):
@@ -22,20 +25,12 @@ cdef scalar linear_form_m4(int n, double *wt, FuncReal *u, GeomReal
         *e, ExtDataReal *ext):
     return -4*int_v(n, wt, u)
 
-cdef c_Ord _order_bf(int n, double *wt, FuncOrd *u, FuncOrd *v, GeomOrd
-        *e, ExtDataOrd *ext):
-    # XXX: with 9 it doesn't shout about the integration order, but gives wrong
-    # results...
-    return create_Ord(20)
-
 cdef c_Ord _order_lf(int n, double *wt, FuncOrd *u, GeomOrd
         *e, ExtDataOrd *ext):
-    # XXX: with 9 it doesn't shout about the integration order, but gives wrong
-    # results...
-    return create_Ord(20)
+    return int_v_ord(n, wt, u)
 
 def set_forms(WeakForm wf, int k=2):
-    wf.thisptr.add_biform(0, 0, &bilinear_form, &_order_bf)
+    wf.thisptr.add_biform(0, 0, &bilinear_form, &bilinear_form_ord)
     if k == 2:
         wf.thisptr.add_liform(0, &linear_form_p2, &_order_lf)
     elif k == -1:
