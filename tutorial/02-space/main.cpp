456c93b66b33ea3b8759a02afea8dd66dea44b91
Pavel Solin <solin@unr.edu>
2009-10-25 19:53:28 -0700

    Work on default BC conditions.

--- tutorial/02-space/main.cpp
+++ tutorial/02-space/main.cpp
@@ -1,13 +1,19 @@
 #include "hermes2d.h"
 
 // This example demonstrates how to create a space over a mesh
-// and how to visualize finite element basis functions. Below,
-// the variable P_INIT defines the initial degree of all mesh
-// elements. Currently, it is set to 3. After visualizing the
-// basis functions using the printed hints , try to change the
-// value of P_INIT, type "make", and run the executable again.
+// and how to visualize the finite element basis functions
+// using the BaseView class. The variable P_INIT defines the 
+// initial degree of all mesh elements. Currently, it is set 
+// to 1. After visualizing the basis functions using the hints 
+// printed in the terminal window, change the value of P_INIT 
+// to 2, 3, etc. to also see higher-order shape functions. 
+//
+// You can use this example to visualize all shape functions 
+// on the reference square and reference triangle domains,
+// just load the corresponding mesh at the beginning of the 
+// main.cpp file. 
 
-int P_INIT = 3;
+int P_INIT = 1;
 
 static char text[] = "\
 Click into the image window and:\n\
@@ -30,15 +36,20 @@ int main(int argc, char* argv[])
 {
   // load the mesh file
   Mesh mesh;
-  mesh.load("domain.mesh");
-  mesh.refine_element(1);           // Sample "manual" element refinement.
+  //mesh.load("domain.mesh");        // original L-shape domain
+  mesh.load("domain_quad.mesh"); // reference square
+  //mesh.load("domain_tri.mesh");  // reference triangle  
+
+  // sample element refinement, to see more basis functions
+  //mesh.refine_all_elements();
 
   // create a shapeset and an H1 space
   H1Shapeset shapeset;
   H1Space space(&mesh, &shapeset);
 
   // assign element orders and initialize the space
-  space.set_uniform_order(P_INIT);  // Set uniform polynomial order P_INIT to all mesh elements.
+  space.set_uniform_order(P_INIT);  // Set uniform polynomial order 
+                                    // P_INIT to all mesh elements.
   space.assign_dofs();              // Enumerate basis functions.
 
   // view the basis functions
