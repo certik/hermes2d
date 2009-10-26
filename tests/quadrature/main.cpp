9175dcd5bbfb1b96d9a4ccbd5a0cb8a50bf0255b
Ondrej Certik <ondrej@certik.cz>
2009-10-25 18:35:16 -0700

    test in C++: quadrature test template added
    Use "make test" to execute C++ tests. Use "make test-python" to test the python
    wrappers.
    
    Signed-off-by: Ondrej Certik <ondrej@certik.cz>
    Signed-off-by: Pavel Solin <solin@unr.edu>

---
+++ tests/quadrature/main.cpp
@@ -0,0 +1,14 @@
+#include "hermes2d.h"
+
+#define ERROR_SUCCESS                               0
+#define ERROR_FAILURE                               -1
+
+int main(int argc, char* argv[])
+{
+  Mesh mesh;
+
+  // if you return this, the test will succeed:
+  return ERROR_SUCCESS;
+  // if you return this, the test will fail:
+  //return ERROR_FAILURE;
+}
