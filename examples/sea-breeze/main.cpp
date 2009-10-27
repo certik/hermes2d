16a19bb2c91c5e6652208225048909d1f4f8267d
Ondrej Certik <ondrej@certik.cz>
2009-10-26 17:58:20 -0700

    domain fixed

--- examples/sea-breeze/main.cpp
+++ examples/sea-breeze/main.cpp
@@ -533,6 +533,8 @@ int main(int argc, char* argv[])
 
   // a-priori mesh refinements
   mesh.refine_all_elements();
+  mesh.refine_all_elements();
+  mesh.refine_all_elements();
   //mesh.refine_towards_boundary(5, 4, false);
   //mesh.refine_towards_boundary(1, 4);
   //mesh.refine_towards_boundary(3, 4);
