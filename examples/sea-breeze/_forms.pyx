60f4de1d3740fa83828d8e3a423fa9232946a9c7
Ondrej Certik <ondrej@certik.cz>
2009-10-28 18:47:10 -0700

    _forms.pyx added

---
+++ examples/sea-breeze/_forms.pyx
@@ -0,0 +1,2 @@
+def register_bc():
+    return
--- examples/sea-breeze/sea-breeze.py
+++ examples/sea-breeze/sea-breeze.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from hermes2d import Mesh, H1Shapeset, PrecalcShapeset, H1Space
+from _forms import register_bc
 
 mesh = Mesh()
 mesh.load("domain-quad.mesh")
@@ -15,3 +16,5 @@ s0 = H1Space(mesh, shapeset_h1)
 s1 = H1Space(mesh, shapeset_h1)
 s3 = H1Space(mesh, shapeset_h1)
 s4 = H1Space(mesh, shapeset_h1)
+
+register_bc(s0, s1, s3, s4)
