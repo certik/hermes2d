89ae08def003d4dec882e1bd6ffdab68c1488b26
Ondrej Certik <ondrej@certik.cz>
2009-10-31 11:53:02 -0700

    register_forms() wrapped

--- examples/sea-breeze/sea-breeze.py
+++ examples/sea-breeze/sea-breeze.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
-from hermes2d import Mesh, H1Shapeset, PrecalcShapeset, H1Space
-from _forms import register_bc
+from hermes2d import (Mesh, H1Shapeset, PrecalcShapeset, H1Space, Solution,
+        WeakForm)
+from _forms import register_bc, register_forms
 
 mesh = Mesh()
 mesh.load("domain-quad.mesh")
@@ -18,3 +19,22 @@ s3 = H1Space(mesh, shapeset_h1)
 s4 = H1Space(mesh, shapeset_h1)
 
 register_bc(s0, s1, s3, s4)
+
+s0.set_uniform_order(1)
+s1.set_uniform_order(1)
+s3.set_uniform_order(1)
+s4.set_uniform_order(1)
+
+ndofs = 0
+ndofs += s0.assign_dofs(ndofs)
+ndofs += s1.assign_dofs(ndofs)
+ndofs += s3.assign_dofs(ndofs)
+ndofs += s4.assign_dofs(ndofs)
+
+w0_prev = Solution()
+w1_prev = Solution()
+w3_prev = Solution()
+w4_prev = Solution()
+
+wf = WeakForm(4)
+register_forms(wf, w0_prev, w1_prev, w3_prev, w4_prev)
