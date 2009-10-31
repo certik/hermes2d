ca9cbc80e49272ed012cfd177f77aafbf66f00dd
Ondrej Certik <ondrej@certik.cz>
2009-10-31 12:55:35 -0700

    use the copy() method

--- examples/sea-breeze/sea-breeze.py
+++ examples/sea-breeze/sea-breeze.py
@@ -75,9 +75,9 @@ for i in range(10):
     w13_view.show(w1_sln, w3_sln)
     w4_view.show(w4_sln, lib="glut")
 
-    w0_prev.copy_by_reference(w0_sln)
-    w1_prev.copy_by_reference(w1_sln)
-    w3_prev.copy_by_reference(w3_sln)
-    w4_prev.copy_by_reference(w4_sln)
+    w0_prev.copy(w0_sln)
+    w1_prev.copy(w1_sln)
+    w3_prev.copy(w3_sln)
+    w4_prev.copy(w4_sln)
 
 a = raw_input()
