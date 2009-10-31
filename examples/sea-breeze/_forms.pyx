efe2257046a68f47bea40a3199d4f315542fb073
Ondrej Certik <ondrej@certik.cz>
2009-10-31 11:09:42 -0700

    Use the correct register_bc()

--- examples/sea-breeze/_forms.pyx
+++ examples/sea-breeze/_forms.pyx
@@ -1,2 +1,9 @@
-def register_bc():
-    return
+from hermes2d._hermes2d cimport H1Space, c_H1Space
+
+cdef extern from "forms.h":
+    void c_register_bc "register_bc"(c_H1Space s0, c_H1Space s1, c_H1Space s3,
+            c_H1Space s4)
+
+def register_bc(H1Space s0, H1Space s1, H1Space s3, H1Space s4):
+    c_register_bc((s0.thisptr)[0], (s1.thisptr)[0], (s3.thisptr)[0],
+            (s4.thisptr)[0])
