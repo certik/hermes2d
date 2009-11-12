bed9f10e2b3f569bca818248ee23bd5063fd076e
Ondrej Certik <ondrej@certik.cz>
2009-11-12 14:47:02 -0800

    Use T_0 = 300.5K

--- examples/sea-breeze/params.h
+++ examples/sea-breeze/params.h
@@ -4,11 +4,11 @@
 // Empirical relations of initial distributions valid for 0 <= z <= 10km
 // "z" in p_z and T_z is in "m", so everything is in SI units
 // presure p in Pascals (empirical formula)
-#define p_z(z) (100000 - 11.476*(z) + 0.00052954*pow((z),2) - 9.38e-9*pow((z),3))
+#define p_z(z) (100000 - 11.3653418526412*(z) + 0.00052954*pow((z),2) - 9.38e-9*pow((z),3))
 // temperature T in Kelvin (calculated by equations.py)
-#define T_z(z) (297.602407347392 - 0.00668816443401528*(z) + 2.28953333742903e-7*pow((z),2) + 9.61407194208881e-12*pow((z),3))
+#define T_z(z) (300.5 - 0.00615074338860148*(z) + 2.74087847242543e-7*pow((z),2) + 1.25829142493795e-11*pow((z),3))
 // gas density (calculated by equations.py)
-#define rho_z(z) (1.17022632601347 - 0.000107996104684066*(z) + 2.86948142331989e-9*pow((z),2))
+#define rho_z(z) (1.15894233531748 - 0.000107996104684066*(z) + 2.86948142331989e-9*pow((z),2))
 #define R 287.14            // Gas constant
 #define g 9.80665           // gravitational acceleration
 #define c_v 20.8            // specific heat capacity
