aae8dfb0ea944dc0b06fb9e13a1b566e40333d24
Ondrej Certik <ondrej@certik.cz>
2009-11-12 14:13:37 -0800

    Refactor the parameters in forms.cpp into params.h

--- examples/sea-breeze/forms.cpp
+++ examples/sea-breeze/forms.cpp
@@ -4,23 +4,9 @@
 
 const double TAU = 0.01;  // this is in seconds
 
-// Empirical relations of initial distributions valid for 0 <= z <= 10km
-// "z" in p_z and T_z is in "m", so everything is in SI units
-// presure p in Pascals (empirical formula)
-#define p_z(z) (100000 - 11.476*(z) + 0.00052954*(z)*(z) - 9.38e-9*(z)*(z)*(z))
-// temperature T in Kelvin (calculated by equations.py)
-#define T_z(z) (297.602407347392 - 0.00668816443401523*(z) + \
-        2.28953333742906e-7*(z)*(z) - 9.61407194208904e-12*(z)*(z)*(z))
-// gas density (calculated by equations.py)
-#define rho_z(z) (1.17022632601347 - 0.000107996104684066*(z) + \
-        2.86948142331989e-9*(z)*(z))
-#define R 287.14            // Gas constant
-#define g 9.80665           // gravitational acceleration
-
 const double T_0 = T_z(0);
 const double p_0 = p_z(0);
 const double rho_0 = rho_z(0);
-const double c_v = 20.8;            // specific heat capacity
 
 //  boundary markers
 #define marker_bottom 1
