a4f25ec908ff23d6ff80362969e3f391a6933c16
Ondrej Certik <ondrej@certik.cz>
2009-12-06 19:42:12 -0800

    Use the correct BC condition

--- examples/sea-breeze/forms.h
+++ examples/sea-breeze/forms.h
@@ -4,6 +4,7 @@
 #include "hermes2d.h"
 #include "params.h"
 
+void set_iteration(int i);
 void register_bc(H1Space &s0, H1Space &s1, H1Space &s3, H1Space &s4);
 void set_ic(Mesh &mesh, Solution &w0, Solution &w1, Solution &w3, Solution &w4);
 void register_forms(WeakForm &wf, Solution &w0_prev, Solution &w1_prev,
