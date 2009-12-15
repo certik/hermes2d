025482f7deb0e30167515c55284687827006d7ac
Ondrej Certik <ondrej@certik.cz>
2009-12-14 22:26:03 -0800

    Tutorial added to the sphinx docs
    It was converted from the latex tutorial.

--- doc/conf.py
+++ doc/conf.py
@@ -18,12 +18,154 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath(os.path.join("..", "python")))
+sys.path.append(os.path.abspath('exts'))
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'math_dollar']
+
+latex_preamble = r"""
+\usepackage{dsfont}
+\usepackage{braket}
+\usepackage{slashed}
+\def\degrees{^\circ}
+\def\d{{\rm d}}
+
+\def\L{{\mathcal L}}
+\def\H{{\mathcal H}}
+\def\M{{\mathcal M}}
+\def\matrix{}
+\def\fslash#1{#1 \!\!\!/}
+\def\F{{\bf F}}
+\def\R{{\bf R}}
+\def\J{{\bf J}}
+\def\x{{\bf x}}
+\def\y{{\bf y}}
+\def\h{{\rm h}}
+\def\a{{\rm a}}
+\newcommand{\bfx}{\mbox{\boldmath $x$}}
+\newcommand{\bfy}{\mbox{\boldmath $y$}}
+\newcommand{\bfz}{\mbox{\boldmath $z$}}
+\newcommand{\bfv}{\mbox{\boldmath $v$}}
+\newcommand{\bfu}{\mbox{\boldmath $u$}}
+\newcommand{\bfF}{\mbox{\boldmath $F$}}
+\newcommand{\bfJ}{\mbox{\boldmath $J$}}
+\newcommand{\bfU}{\mbox{\boldmath $U$}}
+\newcommand{\bfY}{\mbox{\boldmath $Y$}}
+\newcommand{\bfR}{\mbox{\boldmath $R$}}
+\newcommand{\bfg}{\mbox{\boldmath $g$}}
+\newcommand{\bfc}{\mbox{\boldmath $c$}}
+\newcommand{\bfxi}{\mbox{\boldmath $\xi$}}
+\newcommand{\bfw}{\mbox{\boldmath $w$}}
+\newcommand{\bfE}{\mbox{\boldmath $E$}}
+\newcommand{\bfH}{\mbox{\boldmath $H$}}
+\def\Hcurl{{\bfH({\rm curl})}}
+\def\Hdiv{{\bfH({\rm div})}}
+
+\newcommand{\dd}[2]{\frac{\partial #1}{\partial #2}}
+\newcommand{\dx}{\;\mbox{d}\bfx}
+
+
+%\def\back{\!\!\!\!\!\!\!\!\!\!}
+\def\back{}
+\def\col#1#2{\left(\matrix{#1#2}\right)}
+\def\row#1#2{\left(\matrix{#1#2}\right)}
+\def\mat#1{\begin{pmatrix}#1\end{pmatrix}}
+\def\matd#1#2{\left(\matrix{#1\back0\cr0\back#2}\right)}
+\def\p#1#2{{\partial#1\over\partial#2}}
+\def\cg#1#2#3#4#5#6{({#1},\,{#2},\,{#3},\,{#4}\,|\,{#5},\,{#6})}
+\def\half{{\textstyle{1\over2}}}
+\def\jsym#1#2#3#4#5#6{\left\{\matrix{
+{#1}{#2}{#3}
+{#4}{#5}{#6}
+}\right\}}
+\def\diag{\hbox{diag}}
+
+\font\dsrom=dsrom10
+\def\one{\hbox{\dsrom 1}}
+
+\def\res{\mathop{\mathrm{Res}}}
+
+\def\mathnot#1{\text{"$#1$"}}
+
+
+%See Character Table for cmmib10:
+%http://www.math.union.edu/~dpvc/jsmath/download/extra-fonts/cmmib10/cmmib10.html
+\font\mib=cmmib10
+\def\balpha{\hbox{\mib\char"0B}}
+\def\bbeta{\hbox{\mib\char"0C}}
+\def\bgamma{\hbox{\mib\char"0D}}
+\def\bdelta{\hbox{\mib\char"0E}}
+\def\bepsilon{\hbox{\mib\char"0F}}
+\def\bzeta{\hbox{\mib\char"10}}
+\def\boldeta{\hbox{\mib\char"11}}
+\def\btheta{\hbox{\mib\char"12}}
+\def\biota{\hbox{\mib\char"13}}
+\def\bkappa{\hbox{\mib\char"14}}
+\def\blambda{\hbox{\mib\char"15}}
+\def\bmu{\hbox{\mib\char"16}}
+\def\bnu{\hbox{\mib\char"17}}
+\def\bxi{\hbox{\mib\char"18}}
+\def\bpi{\hbox{\mib\char"19}}
+\def\brho{\hbox{\mib\char"1A}}
+\def\bsigma{\hbox{\mib\char"1B}}
+\def\btau{\hbox{\mib\char"1C}}
+\def\bupsilon{\hbox{\mib\char"1D}}
+\def\bphi{\hbox{\mib\char"1E}}
+\def\bchi{\hbox{\mib\char"1F}}
+\def\bpsi{\hbox{\mib\char"20}}
+\def\bomega{\hbox{\mib\char"21}}
+
+\def\bvarepsilon{\hbox{\mib\char"22}}
+\def\bvartheta{\hbox{\mib\char"23}}
+\def\bvarpi{\hbox{\mib\char"24}}
+\def\bvarrho{\hbox{\mib\char"25}}
+\def\bvarphi{\hbox{\mib\char"27}}
+
+%how to use:
+%$$\alpha\balpha$$
+%$$\beta\bbeta$$
+%$$\gamma\bgamma$$
+%$$\delta\bdelta$$
+%$$\epsilon\bepsilon$$
+%$$\zeta\bzeta$$
+%$$\eta\boldeta$$
+%$$\theta\btheta$$
+%$$\iota\biota$$
+%$$\kappa\bkappa$$
+%$$\lambda\blambda$$
+%$$\mu\bmu$$
+%$$\nu\bnu$$
+%$$\xi\bxi$$
+%$$\pi\bpi$$
+%$$\rho\brho$$
+%$$\sigma\bsigma$$
+%$$\tau\btau$$
+%$$\upsilon\bupsilon$$
+%$$\phi\bphi$$
+%$$\chi\bchi$$
+%$$\psi\bpsi$$
+%$$\omega\bomega$$
+%
+%$$\varepsilon\bvarepsilon$$
+%$$\vartheta\bvartheta$$
+%$$\varpi\bvarpi$$
+%$$\varrho\bvarrho$$
+%$$\varphi\bvarphi$$
+
+%small font
+\font\mibsmall=cmmib7
+\def\bsigmasmall{\hbox{\mibsmall\char"1B}}
+
+\def\Tr{\hbox{Tr}\,}
+\def\Arg{\hbox{Arg}}
+\def\atan{\hbox{atan}}
+"""
+
+pngmath_latex_preamble = latex_preamble
+latex_elements = {"preamble": latex_preamble}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
