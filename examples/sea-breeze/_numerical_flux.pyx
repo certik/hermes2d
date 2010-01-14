d649182980e38f3bc9b26bf765a75d0c3f9efb75
Ondrej Certik <ondrej@certik.cz>
2010-01-14 13:37:57 -0800

    Test matrix inversion

--- examples/sea-breeze/_numerical_flux.pyx
+++ examples/sea-breeze/_numerical_flux.pyx
@@ -8,3 +8,6 @@ cdef extern from "numerical_flux.h":
 
 def matrix_R(int i, int j, double w0, double w1, double w3, double w4):
     return c_matrix_R(i, j, w0, w1, w3, w4)
+
+def matrix_R_inv(int i, int j, double w0, double w1, double w3, double w4):
+    return c_matrix_R_inv(i, j, w0, w1, w3, w4)
