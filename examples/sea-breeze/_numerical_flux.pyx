a9030e3d038d2d7ab9267b1aa5e0061d4280c97f
Ondrej Certik <ondrej@certik.cz>
2010-01-14 13:33:14 -0800

    Return the value

--- examples/sea-breeze/_numerical_flux.pyx
+++ examples/sea-breeze/_numerical_flux.pyx
@@ -7,4 +7,4 @@ cdef extern from "numerical_flux.h":
     double c_matrix_R_inv "matrix_R_inv"(int i, int j, double w0, double w1, double w3, double w4)
 
 def matrix_R(int i, int j, double w0, double w1, double w3, double w4):
-    c_matrix_R(i, j, w0, w1, w3, w4)
+    return c_matrix_R(i, j, w0, w1, w3, w4)
