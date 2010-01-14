bb92c4eae005e7ff446bf6a33caf1d4143bf6776
Ondrej Certik <ondrej@certik.cz>
2010-01-14 13:30:43 -0800

    Add python wrappers to the numerical flux

---
+++ examples/sea-breeze/_numerical_flux.pyx
@@ -0,0 +1,10 @@
+from hermes2d._hermes2d cimport H1Space, c_H1Space, c_WeakForm, c_Solution, \
+        WeakForm, Solution, c_Mesh, Mesh
+
+
+cdef extern from "numerical_flux.h":
+    double c_matrix_R "matrix_R"(int i, int j, double w0, double w1, double w3, double w4)
+    double c_matrix_R_inv "matrix_R_inv"(int i, int j, double w0, double w1, double w3, double w4)
+
+def matrix_R(int i, int j, double w0, double w1, double w3, double w4):
+    c_matrix_R(i, j, w0, w1, w3, w4)
