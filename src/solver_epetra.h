3e2ed92b64b3fea2b2db9873545d404c29424c76
David Andrs <andrsd@gmail.com>
2010-01-14 16:25:51 -0800

    Adding support for complex numbers and Trilinos
    Signed-off-by: David Andrs <andrsd@gmail.com>

--- src/solver_epetra.h
+++ src/solver_epetra.h
@@ -58,6 +58,9 @@ protected:
 	Epetra_BlockMap *std_map;
 	Epetra_CrsGraph *grph;
 	Epetra_CrsMatrix *mat;
+#ifdef COMPLEX
+	Epetra_CrsMatrix *mat_im;		// imaginary part of the matrix, mat holds the real part
+#endif
 	bool owner;
 #endif
 
@@ -101,6 +104,9 @@ protected:
 #ifdef HAVE_EPETRA
 	Epetra_BlockMap *std_map;
 	Epetra_Vector *vec;
+#ifdef COMPLEX
+	Epetra_Vector *vec_im;		// imaginary part of the vector, vec holds the real part
+#endif
 	bool owner;
 #endif
 
