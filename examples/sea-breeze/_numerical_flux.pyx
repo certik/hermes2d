3763cc2f74fe8e2eb2488fc6570946b0911be57d
Ondrej Certik <ondrej@certik.cz>
2010-01-15 19:53:41 -0800

    Do things in Python.

--- examples/sea-breeze/_numerical_flux.pyx
+++ examples/sea-breeze/_numerical_flux.pyx
@@ -6,6 +6,11 @@ cdef extern from "numerical_flux.h":
     double c_matrix_R "matrix_R"(int i, int j, double w0, double w1, double w3, double w4)
     double c_matrix_R_inv "matrix_R_inv"(int i, int j, double w0, double w1, double w3, double w4)
     double c_matrix_D_minus "matrix_D_minus"(int i, int j, double w0, double w1, double w3, double w4)
+    double c_R "R"
+    double c_c_v "c_v"
+
+R = c_R
+c_v = c_c_v
 
 def matrix_R(int i, int j, double w0, double w1, double w3, double w4):
     return c_matrix_R(i, j, w0, w1, w3, w4)
