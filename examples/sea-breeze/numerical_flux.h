dd8c8d54258fa8486ca9d338d619e1e9a8d150fa
Ondrej Certik <ondrej@certik.cz>
2010-01-16 18:36:25 -0800

    Call the Riemann solver

--- examples/sea-breeze/numerical_flux.h
+++ examples/sea-breeze/numerical_flux.h
@@ -7,6 +7,8 @@
 double matrix_R(int i, int j, double w0, double w1, double w3, double w4);
 double matrix_R_inv(int i, int j, double w0, double w1, double w3, double w4);
 double matrix_D_minus(int i, int j, double w0, double w1, double w3, double w4);
+void T_rot(double result[4][4], double beta);
+void dot_vector(double result[4], double A[4][4], double B[4]);
 void flux_riemann(double result[4], double w_l[4], double w_r[4]);
 void flux_riemann_invert(double result[4], double w_l[4], double w_r[4]);
 
