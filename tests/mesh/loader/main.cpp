38ab710f90dbda17f13bea972e0eaedb591f0803
David Andrs <andrsd@gmail.com>
2010-01-24 11:21:04 -0800

    Fixing regression in Mesh::load_str()
    Added test 2 cases for load_old() and load_str().

--- tests/mesh/loader/main.cpp
+++ tests/mesh/loader/main.cpp
@@ -3,31 +3,8 @@
 #define ERROR_SUCCESS                               0
 #define ERROR_FAILURE                               -1
 
-int main(int argc, char* argv[])
+void dump_mesh(const Mesh &mesh)
 {
-  int ret = ERROR_FAILURE;
-
-  if (argc < 2)
-  {
-    printf("please input as this format: <mesh type> <meshfile> \n");
-    return ERROR_FAILURE;
-  }
-
-  char *mtype = argv[1];
-  char *file_name = argv[2];
-
-  // load the mesh file
-  Mesh mesh;
-  MeshLoader *mloader = NULL;
-  if (strcmp(mtype, "exII") == 0) mloader = new ExodusIIReader();
-  else if (strcmp(mtype, "h2d") == 0) mloader = new H2DReader();
-  else {
-    printf("failed: unknown mesh loader type\n");
-    return ERROR_FAILURE;
-  }
-
-  if (mloader->load(file_name, &mesh))
-  {
     int ne = mesh.get_num_elements();
     printf("Elements = %d\n", ne);
     for (int eid = 0; eid < ne; eid++)
@@ -61,7 +38,78 @@ int main(int argc, char* argv[])
     }
 
     // TODO: check curvilinear edges (how?)
+}
 
+int main(int argc, char* argv[])
+{
+  int ret = ERROR_FAILURE;
+
+  if (argc < 2)
+  {
+    printf("please input as this format: <mesh type> <meshfile> \n");
+    return ERROR_FAILURE;
+  }
+
+  char *mtype = argv[1];
+  char *file_name = argv[2];
+
+  // load the mesh file
+  Mesh mesh;
+  MeshLoader *mloader = NULL;
+  if (strcmp(mtype, "exII") == 0) mloader = new ExodusIIReader();
+  else if (strcmp(mtype, "h2d") == 0) mloader = new H2DReader();
+  else if (strcmp(mtype, "h2d-str") == 0) {
+    // load the file into a string
+    FILE *file = fopen(file_name , "rb");
+    if (file == NULL) {
+	  fputs("File error", stderr);
+	  exit(1);
+	}
+
+    // obtain file size:
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    rewind(file);
+
+    // allocate memory to contain the whole file:
+    char *buffer = (char *) malloc (sizeof(char) * size);
+    if (buffer == NULL) {
+	  fputs("Memory error", stderr);
+	  exit(2);
+	}
+
+    // copy the file into the buffer:
+    size_t result = fread(buffer, 1, size, file);
+    if (result != size) {
+	  fputs("Reading error", stderr);
+	  exit(3);
+	}
+
+	fclose(file);
+
+    //
+    H2DReader *hloader = new H2DReader();
+	hloader->load_str(buffer, &mesh);
+    dump_mesh(mesh);
+    delete hloader;
+	free(buffer);
+    return ERROR_SUCCESS;
+  }
+  else if (strcmp(mtype, "h2d-old") == 0) {
+    H2DReader *hloader = new H2DReader();
+    hloader->load_old(file_name, &mesh);
+    dump_mesh(mesh);
+    delete hloader;
+    return ERROR_SUCCESS;
+  }
+  else {
+    printf("failed: unknown mesh loader type\n");
+    return ERROR_FAILURE;
+  }
+
+  if (mloader->load(file_name, &mesh))
+  {
+    dump_mesh(mesh);
     ret = ERROR_SUCCESS;
   }
   else
