de984d5434924ffa5f6f0740b469d0b1e6f1ae0e
Pavel Solin <solin@unr.edu>
2010-02-04 00:49:11 -0800

    Updated examples bessel, screen, and waveguide.

--- benchmarks/screen/main.cpp
+++ benchmarks/screen/main.cpp
@@ -21,7 +21,11 @@
 //
 // The following parameters can be changed:
 
-const int P_INIT = 1;             // Initial polynomial degree of all mesh elements.
+const int P_INIT = 1;             // Initial polynomial degree. NOTE: The meaning is different from 
+                                  // standard continuous elements in the space H1. Here, P_INIT refers
+                                  // to the maximum poly order of the tangential component, and polynomials
+                                  // of degree P_INIT + 1 are present in element interiors. P_INIT = 0
+                                  // is for Whitney elements. 
 const double THRESHOLD = 0.5;     // This is a quantitative parameter of the adapt(...) function and
                                   // it has different meanings for various adaptive strategies (see below).
 const int STRATEGY = 1;           // Adaptive strategy:
@@ -50,7 +54,7 @@ const int MESH_REGULARITY = -1;   // Maximum allowed level of hanging nodes:
                                   // their notoriously bad performance.
 const double ERR_STOP = 0.1;      // Stopping criterion for adaptivity (rel. error tolerance between the
                                   // fine mesh and coarse mesh solution in percent).
-const int NDOF_STOP = 40000;      // Adaptivity process stops when the number of degrees of freedom grows
+const int NDOF_STOP = 50000;      // Adaptivity process stops when the number of degrees of freedom grows
                                   // over this limit. This is to prevent h-adaptivity to go on forever.
 
 // problem constants
@@ -113,6 +117,13 @@ int main(int argc, char* argv[])
   ScalarView Yview_i("Electric field Y - imag", 1380, 0, 450, 420);
   OrderView  ord("Polynomial Orders", 0, 460, 450, 420);
 
+  /*
+  // view the basis functions
+  VectorBaseView bview;
+  vbview.show(&space);
+  vbview.wait_for_keypress();
+  */
+
   // matrix solver
   UmfpackSolver solver;
 
@@ -144,8 +155,8 @@ int main(int argc, char* argv[])
     // calculating error wrt. exact solution
     Solution ex;
     ex.set_exact(&mesh, exact);
-    double error = 100 * hcurl_error(&sln_coarse, &ex);
-    info("Exact solution error: %g%%", error);
+    double err_exact = 100 * hcurl_error(&sln_coarse, &ex);
+    info("Exact solution error: %g%%", err_exact);
 
     // visualization
     RealFilter real(&sln_coarse);
@@ -174,25 +185,27 @@ int main(int argc, char* argv[])
 
     // calculate error estimate wrt. fine mesh solution
     HcurlOrthoHP hp(1, &space);
-    double err_est = hp.calc_error(&sln_coarse, &sln_fine) * 100;
-    info("Error estimate: %g%%", err_est);
+    double err_est_adapt = hp.calc_error(&sln_coarse, &sln_fine) * 100;
+    double err_est_hcurl = hcurl_error(&sln_coarse, &sln_fine) * 100;
+    info("Error estimate (adapt): %g%%", err_est_adapt);
+    info("Error estimate (hcurl): %g%%", err_est_hcurl);
 
     // add entries to DOF convergence graphs
-    graph_dof_exact.add_values(space.get_num_dofs(), error);
+    graph_dof_exact.add_values(space.get_num_dofs(), err_exact);
     graph_dof_exact.save("conv_dof_exact.dat");
-    graph_dof_est.add_values(space.get_num_dofs(), err_est);
+    graph_dof_est.add_values(space.get_num_dofs(), err_est_hcurl);
     graph_dof_est.save("conv_dof_est.dat");
 
     // add entries to CPU convergence graphs
-    graph_cpu_exact.add_values(cpu, error);
+    graph_cpu_exact.add_values(cpu, err_exact);
     graph_cpu_exact.save("conv_cpu_exact.dat");
-    graph_cpu_est.add_values(cpu, err_est);
+    graph_cpu_est.add_values(cpu, err_est_hcurl);
     graph_cpu_est.save("conv_cpu_est.dat");
 
-    // if err_est too large, adapt the mesh
-    if (err_est < ERR_STOP) done = true;
+    // if err_est_adapt too large, adapt the mesh
+    if (err_est_adapt < ERR_STOP) done = true;
     else {
-      hp.adapt(THRESHOLD, STRATEGY, ADAPT_TYPE, ISO_ONLY, MESH_REGULARITY, 3);
+      hp.adapt(THRESHOLD, STRATEGY, ADAPT_TYPE, ISO_ONLY, MESH_REGULARITY);
       ndofs = space.assign_dofs();
       if (ndofs >= NDOF_STOP) done = true;
     }
--- examples/waveguide/main.cpp
+++ examples/waveguide/main.cpp
@@ -26,8 +26,12 @@
 //
 // The following parameters can be changed:
 
-const int P_INIT = 1;             // Initial polynomial degree of all mesh elements.
-const bool ALIGN_MESH = false;     // if ALIGN_MESH == true, curvilinear elements aligned with the
+const int P_INIT = 1;             // Initial polynomial degree. NOTE: The meaning is different from 
+                                  // standard continuous elements in the space H1. Here, P_INIT refers
+                                  // to the maximum poly order of the tangential component, and polynomials
+                                  // of degree P_INIT + 1 are present in element interiors. P_INIT = 0
+                                  // is for Whitney elements. 
+const bool ALIGN_MESH = false;    // if ALIGN_MESH == true, curvilinear elements aligned with the
                                   // circular load are used, otherwise one uses a non-aligned mesh.
 const double THRESHOLD = 0.3;     // This is a quantitative parameter of the adapt(...) function and
                                   // it has different meanings for various adaptive strategies (see below).
@@ -77,8 +81,8 @@ const double J = 0.0000033333;
 // boundary conditions
 int e_bc_types(int marker)
 {
-  if (marker == 2) return BC_ESSENTIAL; // perfect conductor
-  else return BC_NATURAL; // impedance
+  if (marker == 2) return BC_ESSENTIAL; // perfect conductor BC
+  else return BC_NATURAL;               // impedance BC
 }
 
 // defining load geometry
@@ -146,7 +150,6 @@ Scalar hcurl_form_kappa(int n, double *wt, Func<Scalar> *u, Func<Scalar> *v, Geo
   return int_curl_e_curl_f<Scalar, Scalar>(n, wt, u, v) + sqr(kappa) * int_e_f<Scalar, Scalar>(n, wt, u, v);
 }
 
-
 int main(int argc, char* argv[])
 {
   // load the mesh
@@ -178,11 +181,18 @@ int main(int argc, char* argv[])
   VectorView eview("Electric field",0,0,800, 590);
   OrderView ord("Order", 800, 0, 700, 590);
 
+  /*
+  // view the basis functions
+  VectorBaseView bview;
+  vbview.show(&space);
+  vbview.wait_for_keypress();
+  */
+
   // matrix solver
   UmfpackSolver solver;
 
   // DOF and CPU convergence graphs
-  SimpleGraph graph_dof, graph_cpu;
+  SimpleGraph graph_dof_est, graph_cpu_est;
 
   // adaptivity loop
   int it = 1, ndofs;
@@ -223,20 +233,21 @@ int main(int argc, char* argv[])
     // calculate error estimate wrt. fine mesh solution
     HcurlOrthoHP hp(1, &space);
     hp.set_biform(0, 0, callback(hcurl_form_kappa));
-    double err_est = hp.calc_error(&sln_coarse, &sln_fine) * 100;
-    info("Energy error estimate: %g%%", err_est);
-    info("Hcurl error estimate: %g%%", hcurl_error(&sln_coarse, &sln_fine) * 100);
+    double err_est_adapt = hp.calc_error(&sln_coarse, &sln_fine) * 100;
+    double err_est_hcurl = hcurl_error(&sln_coarse, &sln_fine) * 100;
+    info("Error estimate (adapt): %g%%", err_est_adapt);
+    info("Error estimate (hcurl): %g%%", err_est_hcurl);
 
-    // add entry to DOF convergence graph
-    graph_dof.add_values(space.get_num_dofs(), err_est);
-    graph_dof.save("conv_dof.dat");
+    // add entries to DOF convergence graphs
+    graph_dof_est.add_values(space.get_num_dofs(), err_est_hcurl);
+    graph_dof_est.save("conv_dof_est.dat");
 
-    // add entry to CPU convergence graph
-    graph_cpu.add_values(cpu, err_est);
-    graph_cpu.save("conv_cpu.dat");
+    // add entries to CPU convergence graphs
+    graph_cpu_est.add_values(cpu, err_est_hcurl);
+    graph_cpu_est.save("conv_cpu_est.dat");
 
-    // if err_est too large, adapt the mesh
-    if (err_est < ERR_STOP) done = true;
+    // if err_est_adapt too large, adapt the mesh
+    if (err_est_adapt < ERR_STOP) done = true;
     else {
       hp.adapt(THRESHOLD, STRATEGY, ADAPT_TYPE, ISO_ONLY, MESH_REGULARITY);
       ndofs = space.assign_dofs();
