8f64f59edd2cf158a1ba77f86fc6d2f893f6e698
Pavel Solin <solin@unr.edu>
2010-02-04 00:59:17 -0800

    Rebase was needed.

--- benchmarks/screen/main.cpp
+++ benchmarks/screen/main.cpp
@@ -21,7 +21,7 @@
 //
 // The following parameters can be changed:
 
-const int P_INIT = 1;             // Initial polynomial degree. NOTE: The meaning is different from 
+const int P_INIT = 0;             // Initial polynomial degree. NOTE: The meaning is different from 
                                   // standard continuous elements in the space H1. Here, P_INIT refers
                                   // to the maximum poly order of the tangential component, and polynomials
                                   // of degree P_INIT + 1 are present in element interiors. P_INIT = 0
