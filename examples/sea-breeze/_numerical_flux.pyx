f4e29fc3d3dead2741740174addccc5ceb87b904
Pavel Solin <solin@unr.edu>
2010-02-06 15:22:39 -0800

    convert riemann

--- examples/sea-breeze/_numerical_flux.pyx
+++ examples/sea-breeze/_numerical_flux.pyx
@@ -8,8 +8,8 @@ cdef extern from "numerical_flux.h":
     double c_matrix_R "matrix_R"(int i, int j, double w0, double w1, double w3, double w4)
     double c_matrix_R_inv "matrix_R_inv"(int i, int j, double w0, double w1, double w3, double w4)
     double c_matrix_D_minus "matrix_D_minus"(int i, int j, double w0, double w1, double w3, double w4)
-    void c_flux_riemann "flux_riemann"(double result[4], double w_l[4], double w_r[4])
-    void c_flux_riemann_invert "flux_riemann_invert"(double result[4], double w_l[4], double w_r[4])
+    void c_flux_riemann "riemann_solver"(double result[4], double w_l[4], double w_r[4])
+    void c_flux_riemann_invert "riemann_solver_invert"(double result[4], double w_l[4], double w_r[4])
 
 def matrix_R(int i, int j, double w0, double w1, double w3, double w4):
     return c_matrix_R(i, j, w0, w1, w3, w4)
