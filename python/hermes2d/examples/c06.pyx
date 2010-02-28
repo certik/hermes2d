3c8a7d8aa15a37c52a6299deee17d74e569a8973
Sameer Regmi <regmisk@gmail.com>
2010-02-27 20:58:51 -0800

    Works on python examples with Aayush's help

--- python/hermes2d/examples/c06.pyx
+++ python/hermes2d/examples/c06.pyx
@@ -3,6 +3,19 @@ from hermes2d._hermes2d cimport scalar, FuncReal, GeomReal, WeakForm, \
         FuncOrd, GeomOrd, ExtDataOrd, int_u_v, int_u_v_ord, BC_NATURAL, BC_ESSENTIAL, \
         H1Space
 
+# Boundary condition types
+cdef int bc_type_06(int marker):
+    if marker == 3:
+        return BC_ESSENTIAL
+    else:
+        return BC_NATURAL
+
+# Function values for Dirichlet boundary markers
+cdef scalar bc_values_06(int marker, double x, double y):
+    if marker == 3:
+        return 100.
+    return 0.
+
 cdef scalar bilinear_form(int n, double *wt, FuncReal *u, FuncReal *v, GeomReal
         *e, ExtDataReal *ext):
     return int_grad_u_grad_v(n, wt, u, v)
@@ -18,17 +31,6 @@ cdef scalar linear_form_surf_06(int n, double *wt, FuncReal *v, GeomReal *e,
     if e.marker != 1:
         return 0.
     return 20. * int_v(n, wt, v)
-
-cdef int bc_type_06(int marker):
-    if marker == 3:
-        return BC_ESSENTIAL
-    else:
-        return BC_NATURAL
-
-cdef scalar bc_values_06(int marker, double x, double y):
-    if marker == 3:
-        return 100.
-    return 0.
 
 cdef c_Ord _order_bf(int n, double *wt, FuncOrd *u, FuncOrd *v, GeomOrd
         *e, ExtDataOrd *ext):
--- python/hermes2d/examples/c12.pyx
+++ python/hermes2d/examples/c12.pyx
@@ -7,61 +7,41 @@ import math
 
 # Problem parameters
 
-def a_11(x, y):
+cdef double a_11(double x, double y):
     if (y > 0):
         return 1 + x*x + y*y
     else:
         return 1
 
-def a_22(x, y):
+cdef double a_22(double x, double y):
     if (y > 0):
         return 1
     else:
         return 1 + x*x + y*y
 
-def a_12(x, y):
+cdef double a_12(double x, double y):
     return 1
 
-def a_21(x, y):
+cdef double a_21(double x, double y):
   return 1
 
-def a_1(x, y):
+cdef double a_1(double x, double y):
     return 0.0
 
-def a_2(x, y):
+cdef double a_2(double x, double y):
     return 0.0
 
-def a_0(x, y):
+cdef double a_0(double x, double y):
   return 0.0
 
 cdef double rhs(double x, double y):
     return 1 + x*x + y*y
 
-def g_D(x, y):
+cdef double g_D(double x, double y):
     return -math.cos(math.pi*x)
 
 cdef double g_N(double x, double y):
     return 0
-
-# Boundary condition types
-cdef int bc_types(int marker):
-    if marker == 1:
-        return BC_ESSENTIAL
-    else:
-        return BC_NATURAL
-
-# Dirichlet boundary condition values
-cdef scalar bc_values(int marker, double x, double y):
-    return g_D(x, y)
-
-def set_bc(H1Space space):
-    space.thisptr.set_bc_types(&bc_types)
-    space.thisptr.set_bc_values(&bc_values)
-
-def set_forms(WeakForm wf):
-    wf.thisptr.add_biform(0, 0, &bilinear_form, &bilinear_form_ord, SYM);
-    wf.thisptr.add_liform(0, &linear_form, &linear_form_ord);
-    wf.thisptr.add_liform_surf(0, &linear_form_surf, &linear_form_surf_ord, 2);
 
 # (Volumetric) bilinear form
 cdef scalar bilinear_form(int n, double *wt, FuncReal *u, FuncReal *v, GeomReal *e, ExtDataReal *ext):
@@ -70,7 +50,7 @@ cdef scalar bilinear_form(int n, double *wt, FuncReal *u, FuncReal *v, GeomReal 
         x = e.x[i]
         y = e.y[i]
         result += (a_11(x, y)*u.dx[i]*v.dx[i] + a_12(x, y)*u.dy[i]*v.dx[i] + a_21(x, y)*u.dx[i]*v.dy[i] + a_22(x, y)*u.dy[i]*v.dy[i] + a_1(x, y)*u.dx[i]*v.val[i] + a_2(x, y)*u.dy[i]*v.val[i] + a_0(x, y)*u.val[i]*v.val[i]) * wt[i]
-
+    
     return result
 
 # Integration order for the bilinear form
@@ -84,7 +64,7 @@ cdef scalar linear_form_surf(int n, double *wt, FuncReal *v, GeomReal *e, ExtDat
 
 # Integration order for surface linear form
 cdef c_Ord linear_form_surf_ord(int n, double *wt, FuncOrd *v, GeomOrd *e, ExtDataOrd *ext):
-    #return v.val[0] * e.x[0] * e.x[0]
+    #return v.val[0] * e.x[0] * e.x[0]  
     return create_Ord(20)
 
 # Volumetric linear form (right-hand side)
@@ -95,3 +75,23 @@ cdef scalar linear_form(int n, double *wt, FuncReal *v, GeomReal *e, ExtDataReal
 cdef c_Ord linear_form_ord(int n, double *wt, FuncOrd *v, GeomOrd *e, ExtDataOrd *ext):
     #return v.val[0] * e.x[0] * e.x[0]
     return create_Ord(20)
+
+cdef int bc_types(int marker):
+    if marker == 1:
+        return BC_ESSENTIAL
+    else:
+        return BC_NATURAL
+
+cdef scalar bc_values(int marker, double x, double y):
+    return g_D(x, y)
+
+def set_bc(H1Space space):
+    space.thisptr.set_bc_types(&bc_types)
+    space.thisptr.set_bc_values(&bc_values)
+
+def set_forms(WeakForm wf):
+    wf.thisptr.add_biform(0, 0, &bilinear_form, &bilinear_form_ord, SYM);
+    wf.thisptr.add_liform(0, &linear_form, &linear_form_ord);
+    wf.thisptr.add_liform_surf(0, &linear_form_surf, &linear_form_surf_ord, 2);
+    
+
