e1b637b49c8ef00b280ebed0e24dd4eec2b2ae1a
Ondrej Certik <ondrej@certik.cz>
2010-03-03 13:35:47 -0800

    Only show the welcome message in verbose mode
    Otherwise it interferes with the output of the python tests.

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -135,13 +135,15 @@ Mesh::Mesh() : HashTable()
 
   if (first_call == 1) {
     first_call = 0;
-    printf("\n----------------------------------------------\n");
-    printf("  This is Hermes2D - a C++ library for rapid \n");
-    printf("prototyping of adaptive FEM and hp-FEM solvers\n");
-    printf("     developed by the hp-FEM group at UNR\n");
-    printf("    and distributed under the GPL license.\n");
-    printf("   For more details visit http://hpfem.org/.\n");
-    printf("----------------------------------------------\n");
+    if (verbose_mode) {
+        printf("\n----------------------------------------------\n");
+        printf("  This is Hermes2D - a C++ library for rapid \n");
+        printf("prototyping of adaptive FEM and hp-FEM solvers\n");
+        printf("     developed by the hp-FEM group at UNR\n");
+        printf("    and distributed under the GPL license.\n");
+        printf("   For more details visit http://hpfem.org/.\n");
+        printf("----------------------------------------------\n");
+    }
   }
 }
 
