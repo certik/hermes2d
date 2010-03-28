ed1f6d744e41d95d20ada4e743d67a5cbdfc231c
Ondrej Certik <ondrej@certik.cz>
2010-03-28 02:28:22 -0700

    tangent

--- examples/sea-breeze/fvm.py
+++ examples/sea-breeze/fvm.py
@@ -1,11 +1,13 @@
 from numpy import array
+from numpy.linalg import norm
 
 from hermes2d import Mesh, set_verbose, MeshView
 
 class Edge(object):
 
-    def __init__(self, i, marker, pair):
-        self._pair = pair
+    def __init__(self, i, marker, pair_nodes):
+        self._pair = pair_nodes
+        self._normal = self.calculate_normal(pair_nodes)
         self._elements = [i]
         self._boundary = True
         self._marker = marker
@@ -13,6 +15,13 @@ class Edge(object):
     def set_second_element(self, i):
         self._elements.append(i)
         self._boundary = False
+
+    def calculate_normal(self, pair_nodes):
+        p0 = pair_nodes[0].coord
+        p1 = pair_nodes[1].coord
+        t = array([p1[0]-p0[0], p1[1]-p0[1]])
+        t = t/norm(t)
+        return (t[1], -t[0])
 
     @property
     def boundary(self):
@@ -23,12 +32,16 @@ class Edge(object):
         return self._marker
 
     @property
+    def normal(self):
+        return self._normal
+
+    @property
     def elements(self):
         return self._elements
 
     def __str__(self):
-        s = "elements: %s, boundary: %s, marker: %s" % (self.elements,
-                self.boundary, self.marker)
+        s = "elements: %s, boundary: %s, marker: %s, n=%s" % (self.elements,
+                self.boundary, self.marker, self.normal)
         return s
 
 class Edges(object):
@@ -60,14 +73,15 @@ class Edges(object):
                 jp1 = j + 1
                 if jp1 >= len(nodes_edge):
                     jp1 = 0
-                pair = (nodes_vertex[j].id, nodes_vertex[jp1].id)
+                pair_nodes = (nodes_vertex[j], nodes_vertex[jp1])
+                pair = (pair_nodes[0].id, pair_nodes[1].id)
                 if pair in edges:
                     raise Exception("This edge was already processed")
                 pair_reversed = (pair[1], pair[0])
                 if pair_reversed in edges:
                     edges[pair_reversed].set_second_element(i)
                     continue
-                edges[pair] = Edge(i, ed.marker, pair)
+                edges[pair] = Edge(i, ed.marker, pair_nodes)
         return edges
 
     def __str__(self):
@@ -79,19 +93,22 @@ class Edges(object):
 def main():
     set_verbose(False)
     mesh = Mesh()
+    print "Loading mesh..."
     mesh.load("GAMM-channel.mesh")
     mesh.refine_element(1, 2)
+    #mesh.refine_all_elements()
+    #mesh.refine_all_elements()
+    #mesh.refine_all_elements()
+    #mesh.refine_all_elements()
 
+    print "Constructing edges..."
     nodes = mesh.nodes_dict
     edges = Edges(mesh)
     elements = mesh.elements
     print edges
-    #mesh.refine_all_elements()
-    #mesh.refine_all_elements()
-    #mesh.refine_all_elements()
-    #mesh.refine_all_elements()
+    print "Done."
 
     mview = MeshView()
-    #mview.show(mesh, lib="mpl", method="orders")
+    mview.show(mesh, lib="mpl", method="orders")
 
 main()
