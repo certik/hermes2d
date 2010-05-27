088c41319a8d9b1e4ffda7efaef0c6850872b585
Pavel Solin <solin.pavel@gmail.com>
2010-05-27 04:30:15 -0700

    Example iron-water updated.

--- examples/iron-water/main.cpp
+++ examples/iron-water/main.cpp
@@ -12,7 +12,6 @@ using namespace RefinementSelectors;
 //  It is very similar to example "saphir", the main difference being that the mesh is loaded in
 //  the ExodusII format (created for example by Cubit).
 //
-//
 //  PDE: -div(D(x,y)grad\Phi) + \Sigma_a(x,y)\Phi = Q_{ext}(x,y)
 //  where D(x, y) is the diffusion coefficient, \Sigma_a(x,y) the absorption cross-section,
 //  and Q_{ext}(x,y) external sources
@@ -24,8 +23,8 @@ using namespace RefinementSelectors;
 //
 //  The following parameters can be changed:
 
-const int P_INIT = 1;             // Initial polynomial degree of all mesh elements.
 const int INIT_REF_NUM = 0;       // Number of initial uniform mesh refinements
+const int P_INIT = 1;             // Initial polynomial degree of all mesh elements.
 const double THRESHOLD = 0.6;     // This is a quantitative parameter of the adapt(...) function and
                                   // it has different meanings for various adaptive strategies (see below).
 const int STRATEGY = 0;           // Adaptive strategy:
@@ -40,7 +39,7 @@ const int STRATEGY = 0;           // Adaptive strategy:
 const CandList CAND_LIST = H2D_HP_ANISO; // Predefined list of element refinement candidates. Possible values are
                                          // H2D_P_ISO, H2D_P_ANISO, H2D_H_ISO, H2D_H_ANISO, H2D_HP_ISO,
                                          // H2D_HP_ANISO_H, H2D_HP_ANISO_P, H2D_HP_ANISO.
-                                         // See the Sphinx tutorial (http://hpfem.org/hermes2d/doc/src/tutorial-2.html#adaptive-h-fem-and-hp-fem) for details.
+                                         // See User Documentation for details.
 const int MESH_REGULARITY = -1;   // Maximum allowed level of hanging nodes:
                                   // MESH_REGULARITY = -1 ... arbitrary level hangning nodes (default),
                                   // MESH_REGULARITY = 1 ... at most one-level hanging nodes,
@@ -54,7 +53,7 @@ const double CONV_EXP = 1.0;      // Default value is 1.0. This parameter influe
 const int NDOF_STOP = 60000;      // Adaptivity process stops when the number of degrees of freedom grows
                                   // over this limit. This is to prevent h-adaptivity to go on forever.
 
-// Problem parameters
+// Problem parameters.
 double L = 30;                        // edge of square
 double L0 = 0.75*0.5*L;               // end of first water layer
 double L1 = 0.5*L;                    // end of second water layer
@@ -69,142 +68,140 @@ double D_IRON = 1./(3.*SIGMA_T_IRON);
 double SIGMA_A_WATER = SIGMA_T_WATER - C_WATER*SIGMA_T_WATER;  // absorbing cross-section
 double SIGMA_A_IRON = SIGMA_T_IRON - C_IRON*SIGMA_T_IRON;
 
-// Boundary condition types
+// Materials:
+const int WATER_1 = 1;
+const int WATER_2 = 2;
+const int IRON = 3;
+
+// Boundary condition types.
 BCType bc_types(int marker)
 {
   if (marker == 1) return BC_NATURAL;
   else return BC_ESSENTIAL;
 }
 
-// Dirichlet boundary condition values
+// Essential (Dirichlet) boundary condition values.
 scalar essential_bc_values(int ess_bdy_marker, double x, double y)
 {
   return 0.0;
 }
 
-// Weak forms
+// Weak forms.
 #include "forms.cpp"
 
 int main(int argc, char* argv[])
 {
-  // Load the mesh
+  // Time measurement.
+  TimePeriod cpu_time;
+  cpu_time.tick();
+
+  // Load the mesh.
   Mesh mesh;
   ExodusIIReader mloader;
   if (!mloader.load("iron-water.e", &mesh)) error("ExodusII mesh load failed.");
 
-  // initial uniform mesh refinement
+  // Perform initial uniform mesh refinement.
   for (int i=0; i < INIT_REF_NUM; i++) mesh.refine_all_elements();
 
-  // Initialize the shapeset and the cache
+  // Initialize the shapeset and the cache.
   H1Shapeset shapeset;
   PrecalcShapeset pss(&shapeset);
 
-  // Create finite element space
+  // Create finite element space.
   H1Space space(&mesh, &shapeset);
   space.set_bc_types(bc_types);
   space.set_essential_bc_values(essential_bc_values);
   space.set_uniform_order(P_INIT);
 
-  // Enumerate basis functions
+  // Enumerate basis functions.
   int ndof = assign_dofs(&space);
 
-  // initialize the weak formulation
-  WeakForm wf(1);
-  wf.add_biform(0, 0, bilinear_form_water, bilinear_form_ord, H2D_SYM, 1);
-  wf.add_biform(0, 0, bilinear_form_water, bilinear_form_ord, H2D_SYM, 2);
-  wf.add_biform(0, 0, bilinear_form_iron, bilinear_form_ord, H2D_SYM, 3);
-  wf.add_liform(0, linear_form_source, linear_form_ord, 1);
+  // Initialize the weak formulation
+  WeakForm wf;
+  wf.add_biform(bilinear_form_water, bilinear_form_ord, H2D_SYM, WATER_1);
+  wf.add_biform(bilinear_form_water, bilinear_form_ord, H2D_SYM, WATER_2);
+  wf.add_biform(bilinear_form_iron, bilinear_form_ord, H2D_SYM, IRON);
+  wf.add_liform(linear_form_source, linear_form_ord, WATER_1);
 
-  // Visualize solution and mesh
-  ScalarView sview("Coarse solution", 0, 100, 798, 700);
-  OrderView  oview("Polynomial orders", 800, 100, 798, 700);
+  // Visualize solution and mesh.
+  ScalarView sview("Coarse solution", 0, 0, 400, 350);
+  OrderView  oview("Polynomial orders", 410, 0, 400, 350);
 
-  // Matrix solver
+  // Matrix solver.
   UmfpackSolver solver;
 
-  // DOF and CPU convergence graphs
+  // DOF and CPU convergence graphs.
   SimpleGraph graph_dof, graph_cpu;
 
-  // create a selector which will select optimal candidate
+  // Initialize refinement selector.
   H1ProjBasedSelector selector(CAND_LIST, CONV_EXP, H2DRS_DEFAULT_ORDER, &shapeset);
 
-  // Adaptivity loop
-  int it = 1;
-  bool done = false;
-  TimePeriod cpu_time;
+  // Adaptivity loop:
+  int as = 1; bool done = false;
   Solution sln_coarse, sln_fine;
   do
     {
-    info("---- Adaptivity step %d ---------------------------------------------", it); it++;
+    info("---- Adaptivity step %d:", as);
 
-    // time measurement
-    cpu_time.tick(H2D_SKIP);
-
-    // Solve the coarse mesh problem
+    // Solve the coarse mesh problem.
     LinSystem ls(&wf, &solver);
-    ls.set_spaces(1, &space);
-    ls.set_pss(1, &pss);
+    ls.set_space(&space);
+    ls.set_pss(&pss);
     ls.assemble();
-    ls.solve(1, &sln_coarse);
+    ls.solve(&sln_coarse);
 
-    // Time measurement
+    // Time measurement.
     cpu_time.tick();
 
-    // View the solution and mesh
+    // View the solution and mesh.
     sview.show(&sln_coarse);
     oview.show(&space);
 
-    // time measurement
+    // Skip visualization time. 
     cpu_time.tick(H2D_SKIP);
 
-    // Solve the fine mesh problem
+    // Solve the fine mesh problem.
     RefSystem rs(&ls);
     rs.assemble();
-    rs.solve(1, &sln_fine);
+    rs.solve(&sln_fine);
 
-    // Calculate error estimate wrt. fine mesh solution
+    // Calculate error estimate wrt. fine mesh solution.
     H1Adapt hp(&space);
     hp.set_solutions(&sln_coarse, &sln_fine);
     double err_est = hp.calc_error() * 100;
 
-    // time measurement
-    cpu_time.tick();
+    // Report results.
+    info("ndof_coarse: %d, ndof_fine: %d, err_est: %g%%", 
+         space.get_num_dofs(), rs.get_space(0)->get_num_dofs(), err_est);
 
-    // report results
-    info("Estimate of error: %g%%", err_est);
-
-    // add entry to DOF convergence graph
+    // Add entry to DOF convergence graph.
     graph_dof.add_values(space.get_num_dofs(), err_est);
     graph_dof.save("conv_dof.dat");
 
-    // add entry to CPU convergence graph
+    // Add entry to CPU convergence graph.
     graph_cpu.add_values(cpu_time.accumulated(), err_est);
     graph_cpu.save("conv_cpu.dat");
 
-    // time measurement
-    cpu_time.tick(H2D_SKIP);
-
-    // If err_est too large, adapt the mesh
+    // If err_est too large, adapt the mesh.
     if (err_est < ERR_STOP) done = true;
     else {
-      hp.adapt(&selector, THRESHOLD, STRATEGY, MESH_REGULARITY);
+      done = hp.adapt(&selector, THRESHOLD, STRATEGY, MESH_REGULARITY);
       ndof = assign_dofs(&space);
       if (ndof >= NDOF_STOP) done = true;
     }
 
-    // Time measurement
-    cpu_time.tick();
+    as++;
   }
   while (done == false);
   verbose("Total running time: %g s", cpu_time.accumulated());
 
-  // Show the fine solution - this is the final result
+  // Show the fine solution - the final result.
   sview.set_title("Final solution");
   sview.show(&sln_fine);
   oview.set_title("Final orders");
   oview.show(&space);
 
-  // wait for all views to be closed
+  // Wait for all views to be closed.
   View::wait();
   return 0;
 }
