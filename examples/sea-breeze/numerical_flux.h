18534c306356a1f26e4bf816e285663272c401ed
Ondrej Certik <ondrej@certik.cz>
2010-06-07 17:08:08 -0700

    sea_breeze added

---
+++ examples/sea-breeze/numerical_flux.h
@@ -0,0 +1,17 @@
+#ifndef __HERMES2D_SEA_BREEZE_NUMERICAL_FLUX_H
+#define __HERMES2D_SEA_BREEZE_NUMERICAL_FLUX_H
+
+#include "hermes2d.h"
+#include "params.h"
+
+double matrix_R(int i, int j, double w0, double w1, double w3, double w4);
+double matrix_R_inv(int i, int j, double w0, double w1, double w3, double w4);
+double matrix_D_minus(int i, int j, double w0, double w1, double w3, double w4);
+void T_rot(double result[4][4], double beta);
+void dot_vector(double result[4], double A[4][4], double B[4]);
+void riemann_solver(double result[4], double w_l[4], double w_r[4]);
+void riemann_solver_invert(double result[4], double w_l[4], double w_r[4]);
+void numerical_flux(double result[4], double w_l[4], double w_r[4],
+        double nx, double ny);
+
+#endif
