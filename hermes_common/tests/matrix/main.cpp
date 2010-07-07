30242cb800c6d175a19ba304cd57a0b1b391c342
Pavel Solin <solin.pavel@gmail.com>
2010-07-07 04:28:16 -0700

    Fighting with hermes_common again.

--- hermes_common/tests/matrix/main.cpp
+++ hermes_common/tests/matrix/main.cpp
@@ -88,12 +88,61 @@ void test_matrix3()
 
 }
 
+void test_matrix4()
+{
+    DenseMatrix m(3);
+    m.add(0, 2, 3.5);
+    m.add(1, 2, 4.5);
+    m.add(2, 1, 1.5);
+    m.add(1, 2, 1);
+    m.add(0, 0, 2.5);
+    printf("Dense Matrix\n");
+    m.print();
+
+    printf("----\n");
+    // convert to CSR and CSC
+    CSRMatrix n1(&m);
+    n1.print();
+    /*
+    FIXME: DenseMatrix -> CSCMatrix segfaults...:
+    CSCMatrix n2(&m);
+    n2.print();
+    */
+}
+
+#include "python_api.h"
+
+void test_matrix5()
+{
+    DenseMatrix m(3);
+    m.add(0, 2, 3.5);
+    m.add(1, 2, 4.5);
+    m.add(2, 1, 1.5);
+    m.add(1, 2, 1);
+    m.add(0, 0, 2.5);
+    printf("Dense Matrix\n");
+
+    Python p;
+    p.push("m", c2py_DenseMatrix(&m));
+    p.exec("print m");
+    p.exec("d = m.to_numpy()");
+    p.exec("print d");
+    p.exec("eps = 1e-10");
+    p.exec("assert abs(d[0, 0]-2.5) < eps");
+    p.exec("assert abs(d[0, 2]-3.5) < eps");
+    p.exec("assert abs(d[1, 2]-5.5) < eps");
+    p.exec("assert abs(d[2, 1]-1.5) < eps");
+    p.exec("assert abs(d[0, 1]-0.0) < eps");
+}
+
 int main(int argc, char* argv[])
 {
     try {
         test_matrix1();
         test_matrix2();
         test_matrix3();
+        test_matrix4();
+        test_matrix5();
 
         return ERROR_SUCCESS;
     } catch(std::exception const &ex) {
