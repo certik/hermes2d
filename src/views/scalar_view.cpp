64ba1cbcf2ff1dba6160372fe03f1a8cbec03757
Milan Hanus <mhanus@kma.zcu.cz>
2010-07-18 09:37:08 +0200

    Revert the change of reset_view in ScalarView::show

--- src/views/scalar_view.cpp
+++ src/views/scalar_view.cpp
@@ -195,7 +195,8 @@ void ScalarView::show(MeshFunction* sln, double eps, int item,
   update_layout();
   wait_for_draw();
   // FIXME: find out why this has to be called after wait_for_draw in order for the view to be reset initially.
-  reset_view(true);
+  reset_view(false); // setting true here makes the view always reset after calling 'show'; particularly in the adaptivity process,
+                     // it would disallow the observation of the process from a manually set viewpoint.
   refresh();
 
   verbose("Showing data in view \"%s\"", title.c_str());
