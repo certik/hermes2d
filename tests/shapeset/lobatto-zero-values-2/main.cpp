636fe966f413f95c36ec6848fc3e6c0cda130b82
Ma Zhonghua <mazhonghua83@gmail.com>
2010-08-07 04:32:52 +0800

    Add test for lobatto shape function.  - test zero values on a triangle domain.
    Formatting tests/shapeset/lobatto-zero-values-1.

--- tests/shapeset/lobatto-zero-values-2/main.cpp
+++ tests/shapeset/lobatto-zero-values-2/main.cpp
@@ -12,15 +12,16 @@ int main(int argc, char* argv[])
   // Load the mesh.
   Mesh mesh;
   H2DReader mloader;
-  // We load the mesh on a (-1, 1)^2 domain.
-  mloader.load("ref_square.mesh", &mesh);            
+  // We load the mesh on a triangle [-1,-1][1,-1][-1,1] domain.
+  mloader.load("ref_triangle.mesh", &mesh);            
 
   // Create an H1 space with default shapeset,
   // natural BC, and linear elements.
   H1Space space(&mesh, NULL, NULL, P_INIT);
-  // The type of element, mesh_mode = 4 means a rectangle element.
-  int mesh_mode = 4;
+  // The type of element, mesh_mode = 3 means a triangle element.
+  int mesh_mode = 3;
   int n = get_num_dofs(&space);
+  info("ndof = %d", n);
 
   int *fn_idx = new int [n];
   int m = 0;
@@ -28,13 +29,11 @@ int main(int argc, char* argv[])
   int vertex1 = 0;
   int vertex2 = 1;
   int vertex3 = 2;
-  int vertex4 = 3;
 
   double x = 0.0, y = 0.0, value = 0.0;
   double x1 = -1.0, y1 = -1.0;
   double x2 =  1.0, y2 = -1.0;
-  double x3 =  1.0, y3 =  1.0;
-  double x4 = -1.0, y4 =  1.0;
+  double x3 = -1.0, y3 =  1.0;
 
   info("Testing................");
   // Check vertex functions.
@@ -60,12 +59,6 @@ int main(int argc, char* argv[])
         printf("... Failed\n");
         return ERROR_FAILURE;
       }
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x4, y4, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
       printf("Vertices... Ok\n");
 
       // Edges.
@@ -74,20 +67,6 @@ int main(int argc, char* argv[])
       {
         x = x3 - (j+1)*(x3 - x2)/order;
         y = y3 - (j+1)*(y3 - y2)/order;
-        value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
-        if (value >= EPS)
-        {
-          printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-          printf("... Failed\n");
-          return ERROR_FAILURE;
-        }
-      }
-      printf("... Ok\n");
-      printf("Edge  3.");
-      for (int j = 0; j < order-1; j++)
-      {
-        x = x3 - (j+1)*(x3 - x4)/order;
-        y = y3 - (j+1)*(y3 - y4)/order;
         value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
         if (value >= EPS)
         {
@@ -114,34 +93,14 @@ int main(int argc, char* argv[])
         printf("... Failed\n");
         return ERROR_FAILURE;
       }
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x4, y4, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
       printf("Vertices... Ok\n");
 
       // Edges.
-      printf("Edge  4.");
-      for (int j = 0; j < order-1; j++)
-      {
-        x = x4 - (j+1)*(x4 - x1)/order;
-        y = y4 - (j+1)*(y4 - y1)/order;
-        value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
-        if (value >= EPS)
-        {
-          printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-          printf("... Failed\n");
-          return ERROR_FAILURE;
-        }
-      }
-      printf("... Ok\n");
       printf("Edge  3.");
       for (int j = 0; j < order-1; j++)
       {
-        x = x4 - (j+1)*(x4 - x3)/order;
-        y = y4 - (j+1)*(y4 - y3)/order;
+        x = x3 - (j+1)*(x3 - x1)/order;
+        y = y3 - (j+1)*(y3 - y1)/order;
         value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
         if (value >= EPS)
         {
@@ -168,29 +127,9 @@ int main(int argc, char* argv[])
         printf("... Failed\n");
         return ERROR_FAILURE;
       }
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x4, y4, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
       printf("Vertices... Ok\n");
 
       // Edges.
-      printf("Edge  4.");
-      for (int j = 0; j < order-1; j++)
-      {
-        x = x1 - (j+1)*(x1 - x4)/order;
-        y = y1 - (j+1)*(y1 - y4)/order;
-        value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
-        if (value >= EPS)
-        {
-          printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-          printf("... Failed\n");
-          return ERROR_FAILURE;
-        }
-      }
-      printf("... Ok\n");
       printf("Edge  1.");
       for (int j = 0; j < order-1; j++)
       {
@@ -206,60 +145,6 @@ int main(int argc, char* argv[])
       }
       printf("... Ok\n");
     }    
-
-    if (i == vertex4)
-    {
-      // Vertices.
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x1, y1, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x2, y2, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
-      value = space.get_shapeset()->get_fn_value(fn_idx[i], x3, y3, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
-      printf("Vertices... Ok\n");
-
-      // Edges.
-      printf("Edge  2.");
-      for (int j = 0; j < order-1; j++)
-      {
-        x = x2 - (j+1)*(x2 - x3)/order;
-        y = y2 - (j+1)*(y2 - y3)/order;
-        value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
-        if (value >= EPS)
-        {
-          printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-          printf("... Failed\n");
-          return ERROR_FAILURE;
-        }
-      }
-      printf("... Ok\n");
-      printf("Edge  1.");
-      for (int j = 0; j < order-1; j++)
-      {
-        x = x2 - (j+1)*(x2 - x1)/order;
-        y = y2 - (j+1)*(y2 - y1)/order;
-        value = space.get_shapeset()->get_fn_value(fn_idx[i], x, y, 0);
-        if (value >= EPS)
-        {
-          printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-          printf("... Failed\n");
-          return ERROR_FAILURE;
-        }
-      }
-      printf("... Ok\n");
-    }   
   }
 
   // Check edge functions.
@@ -290,12 +175,6 @@ int main(int argc, char* argv[])
         printf("... Failed\n");
         return ERROR_FAILURE;
       }
-      value = space.get_shapeset()->get_fn_value(fn_idx[m], x4, y4, 0);
-      if (value >= EPS)
-      {
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
       printf("Vertices... Ok\n");
 
       // Edge 1.
@@ -318,21 +197,8 @@ int main(int argc, char* argv[])
         // Check edge 3.
         for (int j = 0; j < order-1; j++)
         {
-          x = x3 - (j+1)*(x3 - x4)/order;
-          y = y3 - (j+1)*(y3 - y4)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
-        // Check edge 4.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x1 - (j+1)*(x1 - x4)/order;
-          y = y1 - (j+1)*(y1 - y4)/order;
+          x = x3 - (j+1)*(x3 - x1)/order;
+          y = y3 - (j+1)*(y3 - y1)/order;
           value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
           if (value >= EPS)
           {
@@ -364,21 +230,8 @@ int main(int argc, char* argv[])
         // Check edge 3.
         for (int j = 0; j < order-1; j++)
         {
-          x = x3 - (j+1)*(x3 - x4)/order;
-          y = y3 - (j+1)*(y3 - y4)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
-        // Check edge 4.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x4 - (j+1)*(x4 - x1)/order;
-          y = y4 - (j+1)*(y4 - y1)/order;
+          x = x3 - (j+1)*(x3 - x1)/order;
+          y = y3 - (j+1)*(y3 - y1)/order;
           value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
           if (value >= EPS)
           {
@@ -407,19 +260,6 @@ int main(int argc, char* argv[])
             return ERROR_FAILURE;
           }
         }
-        // Check edge 4.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x1 - (j+1)*(x1 - x4)/order;
-          y = y1 - (j+1)*(y1 - y4)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
         // Check edge 1.
         for (int j = 0; j < order-1; j++)
         {
@@ -435,59 +275,13 @@ int main(int argc, char* argv[])
         }
         printf("... Ok\n");
       }
-
-      // Edge 4.
-      if (m%mesh_mode == 3)
-      {
-        printf("Egde  4.");
-        // Check edge 1.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x2 - (j+1)*(x2 - x1)/order;
-          y = y2 - (j+1)*(y2 - y1)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
-        // Check edge 2.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x2 - (j+1)*(x2 - x3)/order;
-          y = y2 - (j+1)*(y2 - y3)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
-        // Check edge 3.
-        for (int j = 0; j < order-1; j++)
-        {
-          x = x3 - (j+1)*(x3 - x4)/order;
-          y = y3 - (j+1)*(y3 - y4)/order;
-          value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-          if (value >= EPS)
-          {
-            printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-            printf("... Failed\n");
-            return ERROR_FAILURE;
-          }
-        }
-        printf("... Ok\n");
-      }
     }
   }
 
   // Check bubble functions.
   info("Check bubble functions.");
-  int number_bubble = space.get_shapeset()->get_num_bubbles(H2D_MAKE_QUAD_ORDER(order, order));
-  int *bubble_idx = space.get_shapeset()->get_bubble_indices(H2D_MAKE_QUAD_ORDER(order, order));
+  int number_bubble = space.get_shapeset()->get_num_bubbles(order);
+  int *bubble_idx = space.get_shapeset()->get_bubble_indices(order);
   for (int i = 0; i < number_bubble; i++, m++ )
   {
     fn_idx[m] = bubble_idx[i];
@@ -529,24 +323,8 @@ int main(int argc, char* argv[])
     // Check edge 3 and vertex 3.
     for (int j = 0; j < order; j++)
     {
-      x = x3 - (j)*(x3 - x4)/order;
-      y = y3 - (j)*(y3 - y4)/order;
-      value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
-      if (value >= EPS)
-      {
-        printf("\nx = %f, y = %f, value = %.20lf", x, y, value);
-        printf("... Failed\n");
-        return ERROR_FAILURE;
-      }
-    }
-    printf("... Ok\n");
-
-    printf("Egde 4 and vertex 4.");
-    // Check edge 4 and vertex 4.
-    for (int j = 0; j < order; j++)
-    {
-      x = x4 - (j)*(x4 - x1)/order;
-      y = y4 - (j)*(y4 - y1)/order;
+      x = x3 - (j)*(x3 - x1)/order;
+      y = y3 - (j)*(y3 - y1)/order;
       value = space.get_shapeset()->get_fn_value(fn_idx[m], x, y, 0);
       if (value >= EPS)
       {
