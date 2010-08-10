1b2698cfaf3a5db9da184052491f165726a4eb4e
Erick Santiago <laviticus@sbcglobal.net>
2010-08-10 03:48:39 -0700

    [Patch] This patch fixed the problems of my previous patch "Uploading of New benchmark nist-9 but not yet complete."

--- benchmarks/nist-9/exact_solution.cpp
+++ benchmarks/nist-9/exact_solution.cpp
@@ -8,9 +8,9 @@ static double fndd(double x, double y, double& dx, double& dy)
   double a = pow(x - X_LOC, 2);
   double b = pow(y - Y_LOC, 2);
   double c = sqrt(a + b);
-  double d = (ALPHA*x - 25);
-  double e = (ALPHA*y - 25);
-  double f = (pow(ALPHA * c - 12.5, 2) + 1);
+  double d = (ALPHA*x - 25.0);
+  double e = (ALPHA*y - 25.0);
+  double f = (pow(ALPHA * c - 12.5, 2) + 1.0);
 
   dx = (d/(f * c));
   dy = (e/(f * c));
