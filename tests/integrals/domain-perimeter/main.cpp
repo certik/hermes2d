df63f49855e50de2c6ca4663d0c630d80d37e6fb
Pavel Solin <solin.pavel@gmail.com>
2010-08-28 22:23:30 -0700

    Minor extension of Valmor's test.
    Valmor added to AUTHORS.

--- tests/integrals/domain-perimeter/main.cpp
+++ tests/integrals/domain-perimeter/main.cpp
@@ -17,21 +17,16 @@ using std::endl;
 //  used in the provided set of domain.meshX files in this directory. The
 //  header in the existing mesh files should be helpful.
 
-// If you add a new domain case, put its name in the enum type below. 
-// In addition, create a domain.meshX file; read the existing files to help 
-// you create yours.
-
-enum Cases { SQUARE, QUAD, ANNULUS };
+//  To add more domains to test, edit the CMakeLists.txt file. Note: The 
+//  boundary markers must be 1, 2, 3, or 4, others will be skipped.
 
 //******************************************************************************
 // Controls
 //
 //  The following parameters can be changed:
-//
 
 const int P_INIT = 1;       // Initial polynomial degree of all mesh elements.
 const int INIT_REF_NUM = 2; // Number of initial uniform mesh refinements.
-const Cases CASE = ANNULUS;  // Pick the case from the enum above
 
 //******************************************************************************
 // Helper functions
@@ -52,7 +47,7 @@ BCType bc_types(int marker)
 //------------------------------------------------------------------------------
 // Compute marked boundary length 
 //
-double BdryLength(MeshFunction* meshfn, int bdryMarker)
+double CalculateBoundaryLength(MeshFunction* meshfn, int bdryMarker)
 {
   Quad2D* quad = &g_quad_2d_std;
   meshfn->set_quad_2d(quad);
@@ -96,154 +91,79 @@ double BdryLength(MeshFunction* meshfn, int bdryMarker)
   
   return 0.5 * length; 
 
-} // end of BdryLength()
+} // end of CalculateBoundaryLength()
 
+#define ERROR_SUCCESS                               0
+#define ERROR_FAILURE                               -1
 
 //******************************************************************************
 // Main
 //
 int main(int argc, char* argv[])
 {
- // Load the mesh.
- Mesh mesh;
- H2DReader mloader;
+  if (argc != 3)
+  {
+    printf("Missing mesh filename and domain length as command-line parameters.");
+    return ERROR_FAILURE;
+  }
 
- switch (CASE)
- {
-  case SQUARE:
-   mloader.load("domain.mesh0", &mesh);
-   break;
-  case QUAD:
-   mloader.load("domain.mesh1", &mesh);
-   break;
-  case ANNULUS:
-   mloader.load("domain.mesh2", &mesh);
-   break;
-  default: 
-   mloader.load("domain.mesh0", &mesh);
- }
+  // Load the mesh.
+  Mesh mesh;
+  H2DReader mloader;
+  mloader.load(argv[1], &mesh);
 
- // Perform initial mesh refinements.
- for (int i=0; i<INIT_REF_NUM; i++) mesh.refine_all_elements();
-
-
- // Create H1 space with a default shapeset.
- H1Space space(&mesh, bc_types, NULL, P_INIT);
-
- Solution sln;
- sln.set_zero(&mesh);
-
- double perimeter = 0.0;
- double exactPerimeter = 0.0;
- double l1,l2,l3,l4 = 0.0;
-
- switch (CASE)
- {
-  case SQUARE:
-   // Calculate the length of the four boundaries segments.
-   l1 = BdryLength(&sln, 1);
-   info("Length of bdry 1 = %g\n", l1);
-
-   l2 = BdryLength(&sln, 2);
-   info("Length of bdry 2 = %g\n", l2);
-
-   l3 = BdryLength(&sln, 3);
-   info("Length of bdry 3 = %g\n", l3);
-
-   l4 = BdryLength(&sln, 4);
-   info("Length of bdry 4 = %g\n", l4);
-  
-   perimeter = l1+l2+l3+l4;
-   info("Computed perimeter = %g\n", perimeter);
-
-   // Set exact value from domain.mesh0 file
-   exactPerimeter = 8.0;
-   info("Exact perimeter = %g\n", exactPerimeter);
-
-   break;
-
-  case QUAD:
-   // Calculate the length of the four boundaries segments.
-   l1 = BdryLength(&sln, 1);
-   info("Length of bdry 1 = %g\n", l1);
-
-   l2 = BdryLength(&sln, 2);
-   info("Length of bdry 2 = %g\n", l2);
-
-   l3 = BdryLength(&sln, 3);
-   info("Length of bdry 3 = %g\n", l3);
-
-   l4 = BdryLength(&sln, 4);
-   info("Length of bdry 4 = %g\n", l4);
-  
-   perimeter = l1+l2+l3+l4;
-   info("Computed perimeter = %g\n", perimeter);
-
-   // Set exact value from domain.mesh1 file
-   exactPerimeter = 3.0 + sqrt(5.0) + sqrt(2.0);
-   info("Exact perimeter = %g\n", exactPerimeter);
-
-   break;
-
-  case ANNULUS:
-   // Calculate the length of the four boundaries segments.
-   l1 = BdryLength(&sln, 1);
-   info("Length of bdry 1 = %g\n", l1);
-
-   l2 = BdryLength(&sln, 2);
-   info("Length of bdry 2 = %g\n", l2);
-
-   perimeter = l1+l2;
-   info("Computed perimeter = %g\n", perimeter);
-
-   // Set exact value from domain.mesh2 file
-   exactPerimeter = 2.0 * PI * 1.7;
-   info("Exact perimeter = %g\n", exactPerimeter);
-
-   break;
-
-  default:
-   // The annulus is the default case
-   // Calculate the length of the four boundaries segments.
-   l1 = BdryLength(&sln, 1);
-   info("Length of bdry 1 = %g\n", l1);
-
-   l2 = BdryLength(&sln, 2);
-   info("Length of bdry 2 = %g\n", l2);
-
-   perimeter = l1+l2;
-   info("Computed perimeter = %g\n", perimeter);
-
-   // Set exact value from domain.mesh2 file
-   exactPerimeter = 2.0 * PI * 1.7;
-   info("Exact perimeter = %g\n", exactPerimeter);
-
-   break;
- }
- 
 /* Graphics not allowed on test versions; user may uncomment for debugging; vfda
- // Display the mesh.
- // (100, 0) is the upper left corner position, 600 x 500 is the window size
- MeshView mview("Mesh", 100, 0, 600, 500);
- mview.show(&mesh);
+  // Display the mesh.
+  // (100, 0) is the upper left corner position, 600 x 500 is the window size
+  MeshView mview("Mesh", 100, 0, 600, 500);
+  mview.show(&mesh);
 
- // Wait for the view to be closed.
- View::wait();
+  // Wait for the view to be closed.
+  View::wait();
 */
 
-#define ERROR_SUCCESS                               0
-#define ERROR_FAILURE                               -1
+  double length = atof(argv[2]);
 
- if (fabs(perimeter - exactPerimeter) < 1e-6) {
-   printf("Success!\n");
-   return ERROR_SUCCESS;
- }
- else {
-   printf("Failure!\n");
-   return ERROR_FAILURE;
- }
+  // Perform initial mesh refinements.
+  for (int i=0; i<INIT_REF_NUM; i++) mesh.refine_all_elements();
+
+  // Create H1 space with a default shapeset.
+  H1Space space(&mesh, bc_types, NULL, P_INIT);
+
+  // FIXME: This Solution is artificial here and it should be removed. The 
+  // function CalculateBoundaryLength() should only take a pointer to Mesh and 
+  // a boundary marker as parameters.
+  Solution sln;
+  sln.set_zero(&mesh);
+
+  // Calculate the length of the four boundaries segments.
+  double l1 = CalculateBoundaryLength(&sln, 1);
+  info("Length of boundary 1 = %g\n", l1);
+
+  double l2 = CalculateBoundaryLength(&sln, 2);
+  info("Length of boundary 2 = %g\n", l2);
+
+  double l3 = CalculateBoundaryLength(&sln, 3);
+  info("Length of boundary 3 = %g\n", l3);
+
+  double l4 = CalculateBoundaryLength(&sln, 4);
+  info("Length of boundary 4 = %g\n", l4);
+  
+  double perimeter = l1 + l2 + l3 + l4;
+  info("Computed perimeter = %10.15f\n", perimeter);
+
+  // Set exact value from CMakeLists.txt file
+  info("Exact perimeter = %g\n", length);
+
+  if (fabs(perimeter - length) < 1e-6) {
+    printf("Success!\n");
+    return ERROR_SUCCESS;
+  }
+  else {
+    printf("Failure!\n");
+    return ERROR_FAILURE;
+  }
  
 
- return 0;
-
+  return 0;
 }
